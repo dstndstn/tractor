b9145762112d30dead66da8addd469028fcc138f
Dustin Lang <dstndstn@gmail.com>
2011-01-05 22:57:00 +0000

    start sketching things in...
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@17013 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- hsttractor.py
+++ hsttractor.py
@@ -1,12 +1,29 @@
 
-from tractor import Tractor
+from tractor import *
 
 import numpy as np
 from math import pi
 
+class Flux(float):
+	def copy(self):
+		return Flux(self)
+
+class PixPos(tuple):
+	def copy(self):
+		return PixPos(self[0],self[1])
+
 
 class HSTTractor(Tractor):
-	pass
+
+	def createNewSource(self, img, x, y, ht):
+		# "ht" is the peak height (difference between image and model)
+		# convert to total flux by normalizing by my patch's peak pixel value.
+		x0,y0,patch = img.getPsf().getPointSourcePatch(x, y)
+		#print 'psf patch:', patch.shape
+		#print 'psf patch: max', patch.max(), 'sum', patch.sum()
+		#print 'new source peak height:', ht, '-> flux', ht/patch.max()
+		#ht /= patch.max()
+		return PointSource(PixPos([x,y]), Flux(ht))
 
 
 
@@ -24,36 +41,56 @@ def measure_sky_variance(img):
 if __name__ == '__main__':
 	import pyfits
 
-	# Dealing with cosmic rays is a PITA so use DRZ for now...
-	#P = pyfits.open('jbf108bzq_flt.fits')
-	#img = P[1].data
-	#err = P[2].data
-	#dq  = P[3].data
-
-	P = pyfits.open('jbf108020_drz.fits')
-	img = P[1].data
-	wht = P[2].data
-	ctx = P[3].data
-
-	cut = [slice(1000,1300), slice(2300,2600)]
-	img = img[cut]
-	wht = wht[cut]
-
-	skyvar = measure_sky_variance(img)
-	invvar = wht / np.median(wht) / skyvar
-
 	import matplotlib
 	matplotlib.use('Agg')
 	import pylab as pl
 
-	skymed = np.median(img.ravel())
-	skysig = np.sqrt(skyvar)
-	zrange = np.array([-3.,+10.]) * skysig + skymed
+	if True:
+		P = pyfits.open('jbf108bzq_flt.fits')
+		img = P[1].data
+		err = P[2].data
+		#dq  = P[3].data
 
-	# add in source noise to variance map
-	# problem for the reader:  why *divide* by wht?
-	srcvar = np.maximum(0, (img - skymed) / np.maximum(wht, np.median(wht)*1e-6))
-	invvar = invvar / (1.0 + invvar * srcvar)
+		P = pyfits.open('jbf108bzq_dq1.fits')
+		dq  = P[0].data
+
+		#cut = [slice(900,1200), slice(2300,2600)]
+		cut = [slice(1400,1700), slice(2300,2600)]
+		img = img[cut]
+		err = err[cut]
+		dq  = dq[cut]
+
+		skyvar = measure_sky_variance(img)
+		skymed = np.median(img.ravel())
+		skysig = np.sqrt(skyvar)
+		print 'Estimate sky value', skymed, 'and sigma', skysig
+		zrange = np.array([-3.,+10.]) * skysig + skymed
+		invvar = 1. / (err**2)
+		invvar[dq > 0] = 0
+
+	else:
+		# Dealing with cosmic rays is a PITA so use DRZ for now...
+		P = pyfits.open('jbf108020_drz.fits')
+		img = P[1].data
+		wht = P[2].data
+		ctx = P[3].data
+		cut = [slice(1000,1300), slice(2300,2600)]
+		img = img[cut]
+		wht = wht[cut]
+
+		# HACKs abound in what follows...
+		skyvar = measure_sky_variance(img)
+		invvar = wht / np.median(wht) / skyvar
+
+		skymed = np.median(img.ravel())
+		skysig = np.sqrt(skyvar)
+		zrange = np.array([-3.,+10.]) * skysig + skymed
+
+		# add in source noise to variance map
+		# problem for the reader:  why *divide* by wht?
+		srcvar = np.maximum(0, (img - skymed) / np.maximum(wht, np.median(wht)*1e-6))
+		invvar = invvar / (1.0 + invvar * srcvar)
+
 
 	pl.clf()
 	pl.hist(img.ravel(), bins=np.linspace(zrange[0], zrange[1], 100))
@@ -74,8 +111,57 @@ if __name__ == '__main__':
 	pl.savefig('invvar.png')
 	
 	pl.clf()
+	pl.imshow((img-skymed) * np.sqrt(invvar), interpolation='nearest', origin='lower')
+	#vmin=-3, vmax=10.)
+	pl.hot()
+	pl.colorbar()
+	pl.savefig('chi.png')
+
+	pl.clf()
 	pl.imshow((img-skymed) * np.sqrt(invvar), interpolation='nearest', origin='lower',
 			  vmin=-3, vmax=10.)
 	pl.hot()
 	pl.colorbar()
-	pl.savefig('chi.png')
+	pl.savefig('chi2.png')
+
+
+	# Initialize with a totally bogus Gaussian PSF model.
+	psf = NGaussianPSF([2.0], [1.0])
+
+	# We'll start by working in pixel coords
+	wcs = NullWCS()
+
+	data = Image(data=img, invvar=invvar, psf=psf, wcs=wcs, sky=skymed)
+	
+	tractor = HSTTractor([data])
+
+	if False:
+		X = tractor.getChiImages()
+		chi = X[0]
+		pl.clf()
+		pl.imshow(chi, interpolation='nearest', origin='lower')
+		pl.hot()
+		pl.colorbar()
+		pl.savefig('chi3.png')
+
+	mods = tractor.getModelImages()
+	mod = mods[0]
+
+	pl.clf()
+	pl.imshow(mod, interpolation='nearest', origin='lower',
+			  vmin=zrange[0], vmax=zrange[1])
+	pl.hot()
+	pl.colorbar()
+	pl.savefig('mod0.png')
+
+	tractor.createSource()
+
+	mods = tractor.getModelImages()
+	mod = mods[0]
+
+	pl.clf()
+	pl.imshow(mod, interpolation='nearest', origin='lower',
+			  vmin=zrange[0], vmax=zrange[1])
+	pl.hot()
+	pl.colorbar()
+	pl.savefig('mod1.png')
--- tractor.py
+++ tractor.py
@@ -1,4 +1,6 @@
-
+from math import ceil, floor, pi, sqrt
+import numpy as np
+import random
 
 '''
 Duck types:
@@ -71,14 +73,166 @@ class Image(object):
 '''
 
 
+class Source(object):
+	'''
+	MUST BE HASHABLE!
+	http://docs.python.org/glossary.html#term-hashable
+	'''
+
+	def getPosition(self):
+		pass
+	def getModelPatch(self, img):
+		pass
+
+class PointSource(Source):
+	def __init__(self, pos, flux):
+		self.pos = pos
+		self.flux = flux
+	def __str__(self):
+		return 'PointSource at ' + str(self.pos) + ' with flux ' + str(self.flux)
+	def __repr__(self):
+		return 'PointSource(' + repr(self.pos) + ', ' + repr(self.flux) + ')'
+
+	def copy(self):
+		return PointSource(self.pos.copy(), self.flux.copy())
+
+	def __hash__(self):
+		return (self.pos, self.flux).__hash__()
+	def __eq__(self, other):
+		return hash(self) == hash(other)
+
+	def getPosition(self):
+		return self.pos
+
+	def getModelPatch(self, img):
+		(px,py) = img.getWcs().positionToPixel(self.getPosition())
+		x0,y0,patch = img.getPsf().getPointSourcePatch(px, py)
+		return (x0,y0, self.flux * patch)
+
+
+def randomint():
+	return int(random.random() * (2**32)) #(2**48))
+
+class Image(object):
+	def __init__(self, data=None, invvar=None, psf=None, sky=0, wcs=None):
+		self.data = data
+		self.invvar = invvar
+		self.psf = psf
+		self.sky = sky
+		self.wcs = wcs
+
+	# Any time an attribute is changed, update the "version" number to a random value.
+	def __setattr__(self, name, val):
+		object.__setattr__(self, name, val)
+		#object.__setattr__(self, 'version', randomint())
+		self.setversion(randomint())
+	def setversion(self, ver):
+		object.__setattr__(self, 'version', ver)
+	def getVersion(self):
+		return self.version
+		
+	def getError(self):
+		return np.sqrt(self.invvar)
+	def getImage(self):
+		return self.data
+	def getPsf(self):
+		return self.psf
+	def getWcs(self):
+		return self.wcs
+
+class WCS(object):
+	def positionToPixel(self, pos):
+		pass
+
+# useful when you're using raw pixel positions rather than RA,Decs
+class NullWCS(WCS):
+	def positionToPixel(self, pos):
+		return pos
+	
+class PSF(object):
+	def applyTo(self, image):
+		pass
+
+	# return (x0, y0, patch), a rendering of a point source at the given pixel
+	# coordinate.
+	def getPointSourcePatch(self, px, py):
+		pass
+
+
+class NGaussianPSF(PSF):
+	def __init__(self, sigmas, weights):
+		'''
+		Creates a new N-Gaussian (concentric, isotropic) PSF.
+
+		sigmas: (list of floats) standard deviations of the components
+
+		weights: (list of floats) relative weights of the components;
+		given two components with weights 0.9 and 0.1, the total mass
+		due to the second component will be 0.1.  These values will be
+		normalized so that the total mass of the PSF is 1.0.
+
+		eg,   NGaussianPSF([1.5, 4.0], [0.8, 0.2])
+		'''
+		self.sigmas = sigmas
+		self.weights = weights
+
+	def applyTo(self, image):
+		from scipy.ndimage.filters import gaussian_filter
+		# gaussian_filter normalizes the Gaussian; the output has ~ the
+		# same sum as the input.
+		
+		res = np.zeros_like(image)
+		for s,w in zip(self.sigmas, self.weights):
+			res += w * gaussian_filter(image, s)
+		res /= sum(self.weights)
+		return res
+
+	def getPointSourcePatch(self, px, py):
+		ix = int(round(px))
+		iy = int(round(py))
+		dx = px - ix
+		dy = py - iy
+		# HACK - MAGIC -- N sigma for rendering patches
+		rad = int(ceil(max(self.sigmas) * 5.))
+		sz = 2*rad + 1
+		X,Y = np.meshgrid(np.arange(sz), np.arange(sz))
+		X -= dx + rad
+		Y -= dy + rad
+		patch = np.zeros((sz,sz))
+		x0 = ix - rad
+		y0 = iy - rad
+		R2 = (X**2 + Y**2)
+		for s,w in zip(self.sigmas, self.weights):
+			patch += w / (2.*pi*s**2) * np.exp(-0.5 * R2 / s**2)
+		patch /= sum(self.weights)
+		print 'sum of PSF patch:', patch.sum()
+		return x0,y0,patch
+
+class Cache(dict):
+	pass
+
+class Catalog(list):
+	def __hash__(self):
+		return hash(tuple([hash(x) for x in self]))
+	def deepcopy(self):
+		return Catalog([x.copy() for x in self])
+
+
 class Tractor(object):
 
-	def __init__(self, catalog, image):
+	def __init__(self, image, catalog=[]):
 		'''
-		image: list of Image objects
+		image: list of Image objects (data)
 		catalog: list of Source objects
 		'''
-		pass
+		self.data = image
+		self.catalog = Catalog(catalog)
+
+		self.cache = Cache()
+		self.cachestack = []
+
+	#def getImage(self, imgi):
+	#eturn self.data[imgi]
 
 	def optimizeAtFixedComplexityStep(self):
 		'''
@@ -91,9 +245,84 @@ class Tractor(object):
 
 		-take step (try full step, back off)
 		'''
-		pass
+		print 'Optimizing at fixed complexity'
 
-	def createSources(self):
+	def getModelPatch(self, img, src):
+		#pixpos = img.getWcs().positionToPixel(src.getPosition())
+		return src.getModelPatch(img)#, pixpos)
+
+	# ??
+	def getModelImageNoCache(self, img):
+		mod = np.zeros_like(img.getImage())
+		mod += img.sky
+		# HACK -- add sources...
+		for src in self.catalog:
+			# get model patch for this src in this img?
+			# point sources vs extended
+			# extended sources -- might want to render pre-psf then apply psf in one shot?
+			(x0,y0,patch) = self.getModelPatch(img, src)
+			(ph,pw) = patch.shape
+			mod[y0:y0+ph, x0:x0+pw] += patch
+
+		return mod
+
+	def getModelImage(self, img):
+		# dependencies of this model image:
+		# img.sky, img.psf, img.wcs, sources that overlap.
+		#deps = hash((img.getVersion(), hash(self.catalog)))
+		deps = (img.getVersion(), hash(self.catalog))
+		print 'Model image:'
+		print '  catalog', self.catalog
+		print '  -> deps', deps
+		mod = self.cache.get(deps, None)
+		if mod is not None:
+			print '  Cache hit!'
+		else:
+			mod = self.getModelImageNoCache(img)
+			print 'Caching model image'
+			self.cache[deps] = mod
+		return mod
+
+	def getModelImages(self):
+		mods = []
+		for img in self.data:
+			mod = self.getModelImage(img)
+			mods.append(mod)
+		return mods
+
+	def getChiImages(self):
+		mods = self.getModelImages()
+		chis = []
+		for img,mod in zip(self.data, mods):
+			chis.append((img.getImage() - mod) * img.getError())
+		return chis
+
+	#def findPeaks(self, img, thresh):
+
+	def createNewSource(self, img, x, y, height):
+		return None
+
+	def getLogProb(self):
+		chisq = 0.
+		for i,chi in enumerate(self.getChiImages()):
+			chisq += (chi ** 2).sum()
+		return -0.5 * chisq
+
+	def pushCache(self):
+		self.cachestack.append(self.cache)
+		self.cache = self.cache.copy()
+
+	def mergeCache(self):
+		# drop the top of the stack.
+		self.cachestack.pop()
+
+	def popCache(self):
+		self.cache = self.cachestack.pop()
+
+	#
+	#def startTryUpdate(self):
+
+	def createSource(self):
 		'''
 		-synthesize images
 		-look for "promising" Positions with "positive" residuals
@@ -101,7 +330,61 @@ class Tractor(object):
 		-instantiate new source (Position, flux, PSFType)
 		-local optimizeAtFixedComplexity
 		'''
-		pass
+		for i,chi in enumerate(self.getChiImages()):
+			img = self.data[i]
+			# PSF-correlate
+			sm = img.getPsf().applyTo(chi)
+			# find peaks, create sources
+			#return sm
+
+			# HACK -- magic value 10
+			#pks = self.findPeaks(sm, 10)
+
+			# find single peak pixel, create source
+			I = np.argmax(sm)
+			(H,W) = sm.shape
+			ix = I%W
+			iy = I/W
+			# this is just the peak pixel height...
+			#ht = img.getImage()[iy,ix] - img.getSky()
+			ht = (img.getImage() - self.getModelImage(img))[iy,ix]
+			#ht = img.getImage()[iy,ix] - img.getSky()
+			src = self.createNewSource(img, ix, iy, ht)
+
+			# try adding the new source...
+			#self.startTryUpdate()
+			#self.catalog.append(src)
+			#self.finishTryUpdate()
+
+			pBefore = self.getLogProb()
+			print 'log-prob before:', pBefore
+
+			self.pushCache()
+			oldcat = self.catalog.deepcopy()
+
+			self.catalog.append(src)
+			self.optimizeAtFixedComplexityStep()
+
+			pAfter = self.getLogProb()
+			print 'log-prob after:', pAfter
+
+			if pAfter > pBefore:
+				print 'Keeping new source'
+				self.mergeCache()
+				
+			else:
+				print 'Rejecting new source'
+				self.popCache()
+				# revert the catalog
+				self.catalog = oldcat
+
+			pEnd = self.getLogProb()
+			print 'log-prob at finish:', pEnd
+
+
+		#ims = self.getSyntheticImages()
+		#for model,data in zip(ims, self.data):
+		#		chi
 
 	def modifyComplexity(self):
 		'''
