389a0258e2845600629377705462e00784017a42
Dustin Lang <dstndstn@gmail.com>
2011-01-16 02:16:13 +0000

    move some stuff from hsttractor.py up to tractor.py
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@17075 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- sdsstractor.py
+++ sdsstractor.py
@@ -1,6 +1,13 @@
+if __name__ == '__main__':
+	import matplotlib
+	matplotlib.use('Agg')
+import pyfits
+import pylab as plt
 
-from tractor import Tractor
+from tractor import *
 
+import numpy as np
+from math import pi
 
 
 class SDSSTractor(Tractor):
