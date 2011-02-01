9bc01ebb2047b77747c712c1ba5361ec86eb9dca
David W. Hogg <dwhogg@gmail.com>
2011-02-01 06:45:38 +0000

    tractor: mixtures: gotta love scipy
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@17376 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- mixture_profiles.py
+++ mixture_profiles.py
@@ -2,71 +2,73 @@
 
 import matplotlib
 matplotlib.use('Agg')
+from math import pi as pi
 import pylab as plt
 import numpy as np
+import scipy.optimize as op
 
-def hogg_exp(x):
-    y = np.zeros_like(x)
-    I = x > -20.0
-    y[I] = np.exp(x[I])
+# magic number
+maxradius = 8.
+
+# note wacky normalization because this is for 2-d Gaussians
+# (but only ever called in 1-d).  Wacky!
+def not_normal(x, m, V):
+    return 1. / (2. * pi * V) * np.exp(-0.5 * x**2 / V)
+
+def mixture_of_not_normals(x, pp):
+    K = len(pp)/2
+    y = 0.
+    for k in range(K):
+        y += pp[k] * not_normal(x, 0., pp[k+K])
     return y
 
-# your documentation IS the code
-def hogg_lsqr_mix(x_k_list, x, y):
-    A = np.zeros((len(x),K))
-    for k in range(K):
-        # use numerically stable (?) hogg exps:
-        A[:,k] = x * hogg_exp(-0.5 * x * x / x_k_list[k]**2)
-    ATA = np.dot(np.transpose(A), A)
-    ATb = np.dot(np.transpose(A), y)
-    ATAinv = np.linalg.inv(ATA)
-    X = np.dot(ATAinv, ATb)
-    for k in range(K):
-        # switch back to REAL (not hogg) exps:
-        A[:,k] = x * np.exp(-0.5 * x * x / x_k_list[k]**2)
-    return (X, np.dot(A, X))
+def badness_of_fit_exp(pars):
+    pp = np.exp(pars)
+    x = np.arange(0., maxradius, 0.01)
+    return np.sum((x * mixture_of_not_normals(x, pp)
+                   - x * np.exp(-x))**2)/len(x)
 
-# this code is a HACK because it FIXES the x_k
-def test_exp(K):
-    # MAGIC numbers 20 and 0.001
-    x = np.arange(0.,20.,0.001)
-    yexp = x * np.exp(-x)
-    # MAGIC numbers 5 and 2:
-    x_k_list = 5. * ((np.arange(K) + 0.5) / K)**2
-    A_k_list, ymix = hogg_lsqr_mix(x_k_list, x, yexp)
-    plt.clf()
-    plt.plot(x,yexp,'k-')
-    plt.plot(x,ymix,'r-')
-    plt.xlabel('$r$ with $r_e = 1$')
-    plt.xlim(0., 10.)
-    plt.ylabel('$r\,\exp(-r)$')
-    plt.ylim(-0.1 * max(yexp), 1.1 * max(yexp))
-    plt.title('black: truth / red: %d-Gaussian approximation' % K)
-    plt.savefig('test_exp_%02d.png' % K)
-    return
+def optimize_mixture(K):
+    # first guess
+    amps = np.log(np.random.uniform(size=(K)))
+    vars = np.log((8.0*np.random.uniform(size=(K)))**2)
+    pars = np.append(amps,vars)
+    # optimize
+    func = badness_of_fit_exp
+    print func(pars)
+    print np.exp(pars)
+    newpars = op.fmin_cg(func, pars)
+    print func(newpars)
+    print np.exp(newpars)
+    return (func(newpars), np.exp(newpars))
 
-# this code is a HACK because it FIXES the x_k
-def test_dev(K):
-    # MAGIC numbers 20 and 0.001
-    x = (np.arange(0.,20.,0.001))**4
-    ydev = np.exp(-(x**0.25))
-    # MAGIC numbers 5 and 2:
-    x_k_list = (5. * ((np.arange(K) + 0.5) / K)**2)**4
-    A_k_list, ymix = hogg_lsqr_mix(x_k_list, x, ydev)
+def plot_mixture(pars, fn):
+    x1 = np.arange(0., maxradius, 0.001)
+    y1 = np.exp(-x1)
+    x2 = np.arange(0., maxradius+2., 0.001)
+    y2 = mixture_of_not_normals(x2, pars)
     plt.clf()
-    plt.plot(x,ydev,'k-')
-    plt.plot(x,ymix,'r-')
-    plt.xlabel('$r$ with $r_e = 1$')
-    plt.xlim(0., 1000.)
-    plt.ylabel('$r\,\exp(-r^{1/4})$')
-    plt.ylim(-0.1 * max(ydev), 1.1 * max(ydev))
-    plt.title('black: truth / red: %d-Gaussian approximation' % K)
-    plt.savefig('test_dev_%02d.png' % K)
-    plt.xlim(0., 10.)
-    plt.savefig('test_dev_zoom_%02d.png' % K)
-    return
+    plt.plot(x1, y1, 'k-')
+    plt.plot(x2, y2, 'k-', lw=4, alpha=0.5)
+    plt.xlim(np.min(x2), np.max(x2))
+    plt.ylim(-0.1*np.max(y1), 1.1*np.max(y1))
+    plt.title('K = %d / badness = %e' %
+              (len(pars)/2, badness_of_fit_exp(np.log(pars))))
+    plt.savefig(fn)
 
 if __name__ == '__main__':
-    for K in range(5,20):
-        test_exp(K)
-        test_dev(K)
+    large = 100.
+    ntry = 10
+    for K in range(3,11):
+        badness = large
+        for t in range(ntry):
+            (b, p) = optimize_mixture(K)
+            if b < badness:
+                pars = p
+                badness = b
+        indx = np.argsort(pars[K:K+K])
+        indx = np.append(indx,K+indx)
+        pars = pars[indx]
+        print '----- best at %i is %f' % (K, b)
+        print pars
+        plot_mixture(pars, 'K%02d.png' % K)
