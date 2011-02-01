317f9246d7d3498c133a3d6b5e7cce4d8f949db2
David W. Hogg <dwhogg@gmail.com>
2011-02-01 07:33:35 +0000

    tractor: mixture: dev works too
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@17379 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- mixture_profiles.py
+++ mixture_profiles.py
@@ -15,6 +15,9 @@ maxradius = 8.
 def not_normal(x, m, V):
     return 1. / (2. * pi * V) * np.exp(-0.5 * x**2 / V)
 
+def hogg_dev(x):
+    return np.exp(-1. * (x**0.25))
+
 def mixture_of_not_normals(x, pars):
     K = len(pars)/2
     y = 0.
@@ -29,14 +32,29 @@ def badness_of_fit_exp(lnpars):
     return np.mean((x * mixture_of_not_normals(x, pars)
                     - x * np.exp(-x))**2) * 1e7
 
-def optimize_mixture(K, pars):
-    func = badness_of_fit_exp
+# note magic 1e7
+def badness_of_fit_dev(lnpars):
+    pars = np.exp(lnpars)
+    x = np.arange(0., maxradius, 0.001)
+    return np.mean((x * mixture_of_not_normals(x, pars)
+                    - x * hogg_dev(x))**2) * 1e7
+
+def optimize_mixture(K, pars, model):
+    if model == 'exp':
+        func = badness_of_fit_exp
+    if model == 'dev':
+        func = badness_of_fit_dev
     newlnpars = op.fmin_cg(func, np.log(pars))
     return (func(newlnpars), np.exp(newlnpars))
 
-def plot_mixture(pars, fn):
+def plot_mixture(pars, fn, model):
     x1 = np.arange(0., maxradius, 0.001)
-    y1 = np.exp(-x1)
+    if model == 'exp':
+        y1 = np.exp(-x1)
+        badness = badness_of_fit_exp(np.log(pars))
+    if model == 'dev':
+        y1 = hogg_dev(x1)
+        badness = badness_of_fit_dev(np.log(pars))
     x2 = np.arange(0., maxradius+2., 0.001)
     y2 = mixture_of_not_normals(x2, pars)
     plt.clf()
@@ -44,20 +62,20 @@ def plot_mixture(pars, fn):
     plt.plot(x2, y2, 'k-', lw=4, alpha=0.5)
     plt.xlim(np.min(x2), np.max(x2))
     plt.ylim(-0.1*np.max(y1), 1.1*np.max(y1))
-    plt.title('K = %d / badness = %e' %
-              (len(pars)/2, badness_of_fit_exp(np.log(pars))))
+    plt.title('K = %d / badness = %e' % (len(pars)/2, badness))
     plt.savefig(fn)
 
 if __name__ == '__main__':
     amp = []
     var = []
-    for K in range(1,11):
-        amp = np.append(amp, 0.1)
-        var = np.append(amp, 0.0001)
-        pars = np.append(amp, var)
-        (badness, pars) = optimize_mixture(K, pars)
-        indx = np.argsort(pars[K:K+K])
-        amp = pars[indx]
-        var = pars[K+indx]
-        pars = np.append(amp, var)
-        plot_mixture(pars, 'K%02d.png' % K)
+    for model in ['dev', 'exp']:
+        for K in range(1,11):
+            amp = np.append(amp, 0.1)
+            var = np.append(amp, 0.0001)
+            pars = np.append(amp, var)
+            (badness, pars) = optimize_mixture(K, pars, model)
+            indx = np.argsort(pars[K:K+K])
+            amp = pars[indx]
+            var = pars[K+indx]
+            pars = np.append(amp, var)
+            plot_mixture(pars, 'K%02d_%s.png' % (K, model), model)
