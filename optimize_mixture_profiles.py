840a1ce923196d1d518680e35d7e7c556b76f686
David W. Hogg <dwhogg@gmail.com>
2011-02-18 00:45:52 +0000

    mixture profiles: tweaks
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@17566 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- optimize_mixture_profiles.py
+++ optimize_mixture_profiles.py
@@ -20,6 +20,17 @@ def not_normal(x, V):
 def hogg_dev(x):
 	return np.exp(-1. * (x**0.25))
 
+# not magic 7. and 8.
+def hogg_lup(x):
+	inner = 7.
+	outer = 8.
+	lup = hogg_dev(x)
+	outside = (x > outer)
+	lup[outside] *= 0.
+	middle = (x > inner) * (x < outer)
+	lup[middle] *= (outer - x[middle])
+	return lup
+
 def mixture_of_not_normals(x, pars):
 	K = len(pars)/2
 	y = 0.
@@ -43,11 +54,19 @@ def badness_of_fit_dev(lnpars):
 	return np.mean((mixture_of_not_normals(x, pars)
 					- hogg_dev(x))**2) / 10.**LOG10_SQUARED_DEVIATION
 
+def badness_of_fit_lup(lnpars):
+	pars = np.exp(lnpars)
+	x = np.arange(0., MAX_RADIUS, 0.001)
+	return np.mean((mixture_of_not_normals(x, pars)
+					- hogg_lup(x))**2) / 10.**LOG10_SQUARED_DEVIATION
+
 def optimize_mixture(K, pars, model):
 	if model == 'exp':
 		func = badness_of_fit_exp
 	if model == 'dev':
 		func = badness_of_fit_dev
+	if model == 'lup':
+		func = badness_of_fit_lup
 	newlnpars = op.fmin_bfgs(func, np.log(pars), maxiter=300)
 	return (func(newlnpars), np.exp(newlnpars))
 
@@ -59,20 +78,23 @@ def plot_mixture(pars, prefix, model):
 	if model == 'dev':
 		y1 = hogg_dev(x1)
 		badness = badness_of_fit_dev(np.log(pars))
+	if model == 'lup':
+		y1 = hogg_lup(x1)
+		badness = badness_of_fit_lup(np.log(pars))
 	K = len(pars) / 2
-	x2 = np.arange(0., MAX_RADIUS*1.2, 0.001)
+	x2 = np.arange(0., 2.*MAX_RADIUS, 0.001)
 	y2 = mixture_of_not_normals(x2, pars)
 	plt.clf()
 	plt.plot(x1, y1, 'k-')
-	plt.plot(x2, y2, 'k-', lw=4, alpha=0.5)
+	plt.plot(x2, y2, 'k-', lw=4, alpha=0.25)
 	for k in range(K):
 		plt.plot(x2, pars[k] * not_normal(x2, pars[k+K]), 'k-', alpha=0.5)
 	plt.title(r"%s / $K=%d$ / maximum radius = $%.1f$ / badness = $%.2f\times 10^{%d}$" % (model, len(pars)/2, MAX_RADIUS, badness, LOG10_SQUARED_DEVIATION))
-	plt.xlim(-0.1*np.max(x2), 1.1*np.max(x2))
+	plt.xlim(-0.1*np.max(x1), 1.1*np.max(x1))
 	plt.ylim(-0.1*np.max(y1), 1.1*np.max(y1))
 	plt.savefig(prefix+'_'+model+'.png')
-	plt.clf()
-	plt.xlim(0.003*np.max(x2), 1.5*np.max(x2))
+	plt.loglog()
+	plt.xlim(0.003*np.max(x1), 1.5*np.max(x1))
 	plt.ylim(0.003*np.max(y1), 1.5*np.max(y1))
 	plt.savefig(prefix+'_'+model+'_log.png')
 
@@ -86,7 +108,7 @@ def rearrange_pars(pars):
 # run this (possibly with adjustments to the magic numbers at top)
 # to find different or better mixtures approximations
 def main():
-	for model in ['exp', 'dev']:
+	for model in ['exp', 'dev', 'lup']:
 		amp = np.array([1.0])
 		var = np.array([1.0])
 		pars = np.append(amp, var)
@@ -121,14 +143,13 @@ def main():
 			if bestbadness < 1.:
 				prefix = 'K%02d_MR%02d_LSD%02d' % (K, int(round(MAX_RADIUS)+0.01), -1 * LOG10_SQUARED_DEVIATION)
 				plot_mixture(pars, prefix, model)
-				txtfile = open(prefix + '.txt', "w")
+				txtfile = open(prefix + '_' + model + '.txt', "w")
 				txtfile.write(str(pars))
 				txtfile.close
 				print model
 				break
 
 if __name__ == '__main__':
-	# magic numbers
-	for MAX_RADIUS in [5., 15., 50.]:
-		for LOG10_SQUARED_DEVIATION in [-6, -8, -10]:
+	for MAX_RADIUS in [10., 30.]:
+		for LOG10_SQUARED_DEVIATION in [-2, -4, -6]:
 			main()
