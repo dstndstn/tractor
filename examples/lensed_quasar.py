765639eac99f69eb1c046267f3de0c2f279ea54c
Phil Marshall <dr.phil.marshall@gmail.com>
2011-08-27 21:56:33 +0000

    added guess for naked cusp, cleaned up tests. parity check fails for minor cusps
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@19462 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- examples/lensed_quasar.py
+++ examples/lensed_quasar.py
@@ -27,6 +27,7 @@ import matplotlib.nxutils as nx
 # - einsteinradius  : Einstein radius (some angular units)
 # - gammacos2phi    : external shear amplitude times cos(2 phi)
 # - gammasin2phi    : external shear amplitude times sin(2 phi)
+
 class GravitationalLens:
 
     def __init__(self, position, einsteinradius, gammacos2phi, gammasin2phi):
@@ -78,7 +79,7 @@ class GravitationalLens:
 # ----------------------------------------------------------------------------
 
     # magic number 100
-    # note computational overkill; it's a fucking circle.
+    # note computational overkill; it's a circle.
     def radial_caustic(self):
         eig1, eig2 = self.eigs()
         npts = 100
@@ -121,18 +122,29 @@ class GravitationalLens:
 
 # ----------------------------------------------------------------------------
 
-    # only partially written
-    def guess_imagepositions(self, sourceposition):
+    # input: a single source position, shape (2)
+    # output: a first guess at N image positions, shape (N, 2)
+    def guess_image_positions(self, sourceposition):
         N = self.number_of_images(sourceposition)
         assert(N > 0 and N < 5)
         if N == 1:
             ipos = sourceposition.copy()
         if N == 2:
-            dpos = sourceposition - self.position
+            dpos = sourceposition.copy() - self.position
+            # radial vector, length = thetaE
             blah = self.einsteinradius * dpos / np.sqrt(np.dot(dpos, dpos))
             ipos = np.array([sourceposition.copy() + blah, sourceposition.copy() - blah])
         if N == 3:
-            assert(False)
+            dpos = sourceposition.copy() - self.position
+            # tangential vector, length = 5*thetaE to cope with very 
+            # wide separation arcs...
+            blah = 5.0*self.einsteinradius * dpos / np.sqrt(np.dot(dpos, dpos))
+            rot = np.array([[0,1],[-1,0]])
+            wffl = np.dot(rot,blah)
+            ipos = np.array([sourceposition.copy() - wffl, 
+                             sourceposition.copy(), 
+                             sourceposition.copy() + wffl])
+            # print "guessed image position:",ipos
         if N == 4:
             ipos = self.cross()
         return ipos
@@ -142,38 +154,59 @@ class GravitationalLens:
     # input: a single source position shape (2) and a first guess at N image positions shape (N, 2)
     # output: N image positions shape (N, 2)
     # MAGIC NUMBER: tol
-    def refine_imagepositions(self, sourceposition, guessedimagepositions, tol=1.e-20):
+    def refine_image_positions(self, sourceposition, guessedimagepositions, tol=1.e-20):
         ipos = np.atleast_2d(guessedimagepositions)
         parities = self.parities(ipos)
         N = len(ipos)
-        dspos = np.outer(np.ones(N), sourceposition) - self.sourcepositions(ipos)
+        dspos = np.outer(np.ones(N), sourceposition) - self.source_positions(ipos)
         i = 0
         while np.sum(dspos**2) > tol:
             i += 1
-            dipos = np.array([np.dot(tens, dsp) for tens, dsp in zip(self.magnificationtensors(ipos), dspos)])
+            dipos = np.array([np.dot(tens, dsp) for tens, dsp in zip(self.magnification_tensors(ipos), dspos)])
             ipos = ipos + dipos
-            dspos = np.outer(np.ones(N), sourceposition) - self.sourcepositions(ipos)
+            dspos = np.outer(np.ones(N), sourceposition) - self.source_positions(ipos)
+            # print "refine_image_positions:",ipos,self.parities(ipos)
+                            
         return ipos
 
 # ----------------------------------------------------------------------------
 
-    # read the source code
-    def imagepositions(self, sourceposition):
+    # solve for image positions given source position.
+    # input: source position, shape (2,)
+    # output: image positions, shape (N,2)
+    def image_positions(self, sourceposition):
         assert(sourceposition.shape == (2, ))
-        ipos = self.guess_imagepositions(sourceposition)
-        ipos = self.refine_imagepositions(sourceposition, ipos)
-        N = len(ipos)
+        
+        ipos = self.guess_image_positions(sourceposition)
+        N = self.number_of_images(sourceposition)
+        initialparities = self.parities(ipos)
+                
+        ipos = self.refine_image_positions(sourceposition, ipos)
+        
+        fail = False
         if (N == 2) or (N == 4):
             if (np.sum(self.parities(ipos)) != 0):
-                print 'imagepositions: WARNING: parities wrong'
-        return 
+                print 'image_positions: WARNING: parities wrong, some images have either merged or collapsed:'
+                print 'image positions:',ipos
+                print 'image magnifications:',self.magnifications(ipos)
+                fail = True
+        elif (N == 3):
+            if (np.sum(self.parities(ipos) - initialparities) != 0):
+                print 'image_positions: WARNING: parities wrong, some images have merged or collapsed:'
+                print 'image positions:',ipos
+                print 'image magnifications:',self.magnifications(ipos)
+                fail = True
+                
+        # Try refining again from a different guess?                  
+                
+        return ipos,fail
 
 # ----------------------------------------------------------------------------
 
     # input: image positions shape (N, 2)
     # output: source position
     # note outer, sqrt, sum craziness
-    def sourcepositions(self, imagepositions):
+    def source_positions(self, imagepositions):
         ipos = np.atleast_2d(imagepositions)
         dpos = ipos - np.outer(np.ones(len(ipos)), self.position)
         r = np.outer(np.sqrt(np.sum(dpos * dpos, axis=1)), [1,1])
@@ -188,11 +221,12 @@ class GravitationalLens:
  # ----------------------------------------------------------------------------
 
     # output shape (N, 2, 2)
-    def inversemagnificationtensors(self, imagepositions):
+    def inverse_magnification_tensors(self, imagepositions):
         ipos = np.atleast_2d(imagepositions)
         mag = np.zeros((len(ipos), 2, 2))
         mag[:,0,0] = 1.
         mag[:,1,1] = 1.
+        # print "inverse_magnification_tensors: ipos = ",ipos
         dpos = ipos - self.position
         rcubed = np.sum(dpos * dpos, axis=1)**1.5
         if np.min(rcubed) <= 0.:
@@ -211,14 +245,12 @@ class GravitationalLens:
         mag[:,1,1] += self.gammacos2phi
         return mag
 
- # ----------------------------------------------------------------------------
-
-    def magnificationtensors(self, imagepositions):
-        return np.array([np.linalg.inv(t) for t in self.inversemagnificationtensors(imagepositions)])
+    def magnification_tensors(self, imagepositions):
+        return np.array([np.linalg.inv(t) for t in self.inverse_magnification_tensors(imagepositions)])
 
     # crazy if you run this and magnificationtensors in the same code; think caching
     def magnifications(self, imagepositions):
-        return 1. / np.array(map(np.linalg.det, self.inversemagnificationtensors(imagepositions)))
+        return 1. / np.array(map(np.linalg.det, self.inverse_magnification_tensors(imagepositions)))
 
     # crazy if you run this and magnificationtensors in the same code; think caching
     def parities(self, imagepositions):
@@ -233,9 +265,9 @@ class GravitationalLens:
         def ln_Gaussian_1d_zeromean(x, var):
             return -0.5 * np.log(2. * np.pi * var) - 0.5 * x**2 / var
         assert(len(imagepositions) == 4)
-        sourcepositions = self.sourcepositions(imagepositions)
+        sourcepositions = self.source_positions(imagepositions)
         meansourceposition = np.mean(sourcepositions, axis=0) # WRONG
-        magtensors = self.magnificationtensors(imagepositions)
+        magtensors = self.magnification_tensors(imagepositions)
         mags = np.array(map(np.linalg.det, magtensors))
         if paritycheck:
             if mags[0] <= 0.:
@@ -275,12 +307,12 @@ class GravitationalLens:
         plt.plot(c[0], c[1], 'k', lw=2.)
         if sourcepositions is not None:
             spos = np.atleast_2d(sourcepositions)
-            print 'plot: plotting spos:', spos
+            # print 'plot: plotting spos:', spos
             plt.scatter(spos[:,0], spos[:,1], c='k', marker='x')
         if imagepositions is not None:
             ipos = np.atleast_2d(imagepositions)
-            mags = np.array(map(np.linalg.det, sis.magnificationtensors(ipos)))
-            s = 10. * np.abs(mags)
+            mags = np.array(map(np.linalg.det, self.magnification_tensors(ipos)))
+            s = 20. * np.sqrt(np.abs(mags))
             I = mags < 0
             if np.sum(I) > 0:
                 plt.scatter(ipos[I,0], ipos[I,1], s=s[I], c='k', marker='o', facecolor='none')
@@ -297,67 +329,139 @@ class GravitationalLens:
 
 if __name__ == '__main__':
         
+    # Initialise lens:
+    
+    # test = 'high_mag'
+    test = 'mergers'
+
+    # config = 'major_cusp'
+    config = 'minor_cusp'
+    # config = 'naked_cusp'
+    print "Requested image configuration is",config
+
     lenspos = [0.5, 0.75]
     b = 1.3 # arcsec
-    gamma = 0.2
+    if config == 'naked_cusp':
+      gamma = 0.5
+    else:
+      gamma = 0.2
     phi = 0.2 # rad
-    sis = GravitationalLens(lenspos, b, gamma * np.cos(2. * phi), gamma * np.sin(2. * phi))
-    nsample = 100
-    ipos = np.zeros((nsample, 2))
-    ipos[:,0] = np.random.uniform(0.5, 1.0, size=nsample)
-    ipos[:,1] = np.random.uniform(-0.6, -1.0, size=nsample)
-    mags = sis.magnifications(ipos)
-    print np.argmax(mags), mags[np.argmax(mags)], ipos[np.argmax(mags)].shape
-    spos = sis.sourcepositions(ipos[np.argmax(mags)])[0]
+    sis = GravitationalLens(lenspos, b, gamma*np.cos(2.0*phi), gamma*np.sin(2.0*phi))
+
     plt.clf()
     sis.plot()
-    plt.savefig('foo.png')
-    plt.clf()
-    ipos = sis.imagepositions(spos)
-    spos2 = sis.sourcepositions(ipos)
-    print spos, spos2, sis.magnifications(ipos)
-    sis.plot(sourcepositions=np.append(spos2, np.atleast_2d(spos), axis=0), imagepositions=ipos)
-    plt.savefig('bar.png')
-
-if False:
-    caustic = sis.tangential_caustic()
-    critcurve = sis.critical_curve()
-    posvar = 1.e-3
-
-    imagefluxes = np.array([10., 10., 10., 10.])
-    magtensors = sis.magnificationtensors(imagepositions)
-    magscalars = np.array([np.linalg.det(ten) for ten in magtensors])
-    chain, lnp = sis.sample_prior(imagepositions, None, posvar, None, 10)
+    foofile = 'foo.png'
+    plt.savefig(foofile)
+    print "Lens outline plotted in",foofile
     
-    plt.clf()
-    for i in range(5):
-        plt.plot(lnp[i,:])
-    plt.xlabel('link number')
-    plt.ylabel('ln prior probability')
-    plt.savefig('lnp.png')
-    for i in range(5):
-        if i == 0:
-            ipos = imagepositions
-            print sis.ln_prior(imagepositions, None, posvar, None)
-        else:
-            ipos = np.reshape(chain[i,:,-1], (4,2))
-        spos = sis.sourcepositions(ipos)
-        plt.clf()
-        plt.subplot(111, aspect='equal')
-        mags = np.abs(map(np.linalg.det, sis.magnificationtensors(ipos)))
-        plt.plot(caustic[:,0], caustic[:,1], 'g')
-        plt.plot(critcurve[:,0], critcurve[:,1], 'b')
-        plt.scatter(ipos[:,0], ipos[:,1], s=mags, c='k', marker='o')
-        plt.scatter(spos[:,0], spos[:,1], c='r', marker='o')
-        xt = 3 * np.random.uniform(size=(3000,2))
-        color = ['m', 'r', 'k', 'g', 'c', 'b']
-        colors = [color[sis.number_of_images(x)] for x in xt]
-        plt.scatter(xt[:,0], xt[:,1], c=colors, marker='o', alpha=0.5)
-        plt.xlim(-2, 3.)
-        plt.ylim(-2, 3.)
-        plt.xlabel('x (arcsec)')
-        plt.ylabel('y (arcsec)')
-        plt.savefig('sky-%02d.png' % i)
+    nsample = 100
+    print "Drawing",nsample,"sample image positions..."
+    ipos = np.zeros((nsample, 2))
 
+    if config == 'minor_cusp':
+      ipos[:,0] = np.random.uniform(1.5, 3.0, size=nsample)
+      ipos[:,1] = np.random.uniform(0.5, 1.5, size=nsample)
+    elif config == 'major_cusp':
+      ipos[:,0] = np.random.uniform(0.5, 1.0, size=nsample)
+      ipos[:,1] = np.random.uniform(-0.6, -1.0, size=nsample)
+    elif config == 'naked_cusp':
+      ipos[:,0] = np.random.uniform(0.5, 1.0, size=nsample)
+      ipos[:,1] = np.random.uniform(-0.7, -2.0, size=nsample)
+    else:
+      ipos[:,0] = np.random.uniform(-2.0, 3.0, size=nsample)
+      ipos[:,1] = np.random.uniform(-2.0, 3.0, size=nsample)
+  
+    if test == 'high_mag':
+      # High mag test - find high magnification image, its source position, and 
+      # then solve for the  image positions.
+      mags = sis.magnifications(ipos)
+      print "Highest mag sample has mag =",mags[np.argmax(mags)]
+      print "Image position is",ipos[np.argmax(mags)]
+      spos = sis.source_positions(ipos[np.argmax(mags)])[0]
+      fail = True
+
+    elif test == 'mergers':
+      # Mergers test - find an image position that does not solve properly, by
+      # testing target and solved image numbers:
+      spoz = sis.source_positions(ipos)
+      nimz = np.ones(nsample)
+      for i in range(nsample):
+        nimz[i] = sis.number_of_images(spoz[i])
+      if config == 'naked_cusp':
+        target = 3
+      else:
+        target = 4
+      index = np.where(nimz == target)
+      ipoz = ipos[index]
+      spoz = spoz[index]
+      fail = False
+      total = len(spoz)
+      count = 0
+      for spos in spoz:
+        ipos,fail = sis.image_positions(spos)
+        count += 1
+        if fail:
+          print "Image solve failure (after ",int(count/(0.01*total)),"% of samples)"
+          break
+
+    if fail:
+      print "Source position is",spos
+      nim = sis.number_of_images(spos)
+      print "No. of expected images:",nim
+      ipos,dummy = sis.image_positions(spos)
+      print "Solved image positions:",ipos
+      print "Image magnifications:",sis.magnifications(ipos)
+      spos2 = sis.source_positions(ipos)
+      print "Corresponding source positions:",spos2
+
+      plt.clf()
+      sis.plot(sourcepositions=np.append(spos2, np.atleast_2d(spos), axis=0), imagepositions=ipos)
+      barfile = 'bar.png'
+      plt.savefig(barfile)
+      print "Images and source(s) plotted in",barfile
+   
+    else:
+      print "All tests passed OK"
+     
+# if False:
+#     caustic = sis.tangential_caustic()
+#     critcurve = sis.critical_curve()
+#     posvar = 1.e-3
+# 
+#     imagefluxes = np.array([10., 10., 10., 10.])
+#     magtensors = sis.magnificationtensors(imagepositions)
+#     magscalars = np.array([np.linalg.det(ten) for ten in magtensors])
+#     chain, lnp = sis.sample_prior(imagepositions, None, posvar, None, 10)
+#     
+#     plt.clf()
+#     for i in range(5):
+#         plt.plot(lnp[i,:])
+#     plt.xlabel('link number')
+#     plt.ylabel('ln prior probability')
+#     plt.savefig('lnp.png')
+#     for i in range(5):
+#         if i == 0:
+#             ipos = imagepositions
+#             print sis.ln_prior(imagepositions, None, posvar, None)
+#         else:
+#             ipos = np.reshape(chain[i,:,-1], (4,2))
+#         spos = sis.sourcepositions(ipos)
+#         plt.clf()
+#         plt.subplot(111, aspect='equal')
+#         mags = np.abs(map(np.linalg.det, sis.magnificationtensors(ipos)))
+#         plt.plot(caustic[:,0], caustic[:,1], 'g')
+#         plt.plot(critcurve[:,0], critcurve[:,1], 'b')
+#         plt.scatter(ipos[:,0], ipos[:,1], s=mags, c='k', marker='o')
+#         plt.scatter(spos[:,0], spos[:,1], c='r', marker='o')
+#         xt = 3 * np.random.uniform(size=(3000,2))
+#         color = ['m', 'r', 'k', 'g', 'c', 'b']
+#         colors = [color[sis.number_of_images(x)] for x in xt]
+#         plt.scatter(xt[:,0], xt[:,1], c=colors, marker='o', alpha=0.5)
+#         plt.xlim(-2, 3.)
+#         plt.ylim(-2, 3.)
+#         plt.xlabel('x (arcsec)')
+#         plt.ylabel('y (arcsec)')
+#         plt.savefig('sky-%02d.png' % i)
+# 
 # ============================================================================
 
