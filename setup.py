2943af5b6ff6c2a90913d422674a394e153abb9c
David W. Hogg <dwhogg@gmail.com>
2012-02-29 19:42:45 +0000

    the tractor: possible setup.py
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@20307 03a3cea6-2c03-0410-ac9b-9271b1c66c29

---
+++ setup.py
@@ -0,0 +1,42 @@
+from distutils.core import setup, Extension
+from numpy.distutils.misc_util import get_numpy_include_dirs
+
+numpy_inc = get_numpy_include_dirs()
+
+c_swig_module_mix = Extension('tractor._mix',
+                              sources = ['tractor/mix_wrap.c' ],
+                              include_dirs = numpy_inc,
+                              extra_objects = [],
+                              undef_macros=['NDEBUG'],
+                              #extra_compile_args=['-O0','-g'],
+                              #extra_link_args=['-O0', '-g'],
+                              )
+
+c_swig_module_em = Extension('tractor._emfit',
+                             sources = ['tractor/emfit_wrap.c' ],
+                             include_dirs = numpy_inc,
+                             extra_objects = [],
+                             undef_macros=['NDEBUG'],
+                             #extra_compile_args=['-O0','-g'],
+                             #extra_link_args=['-O0', '-g'],
+                             )
+
+setup(
+    name="the Tractor",
+    version="n/a",
+    author="Dustin Lang (Princeton) and David W. Hogg (NYU)",
+    author_email="dstn@astro.princeton.edu",
+    packages=["tractor"],
+    ext_modules = [c_swig_module_mix, c_swig_module_em],
+    url="http://theTractor.org/",
+    license="GPLv2",
+    description="probabilistic astronomical image analysis",
+    long_description="Our first attempt at replacing heuristic astronomical catalogs with models built with specified likelihoods, priors, and utilities.",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+)
