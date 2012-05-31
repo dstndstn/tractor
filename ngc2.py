Bug fixes, move zr calculation to info
git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@20804 03a3cea6-2c03-0410-ac9b-9271b1c66c29

ngc2.py
@@ -59,22 +59,27 @@ def main():
 
     TI = []
     sources = []
-    TItemp = []
     for rcf in rcfs:
         try:
             print rcf
 
-            TItemp.extend([st.get_tractor_image(rcf[0], rcf[1], rcf[2], bandname,useMags=True,roiradecsize=(ra,dec,radius)) for bandname in bands])
+            TItemp = [st.get_tractor_image(rcf[0], rcf[1], rcf[2], bandname,useMags=True,roiradecsize=(ra,dec,radius)) for bandname in bands]
             print TItemp
+            print TItemp[0]
+
             timg,info = TItemp[0]
+            if timg is None:
+                print "Zero roi"
+                continue
+
+            print info['roi']
             sources.append(st.get_tractor_sources(rcf[0], rcf[1], rcf[2],bandname,roi=info['roi'],bands=bands))
             TI.extend(TItemp)
-            TItemp = []
         except TypeError:
             print "Sources not in data"
 
-    timg,info = TI[0]
     print TI
+    timg,info = TI[0]
     photocal = timg.getPhotoCal()
 
     wcs = timg.getWcs()
@@ -85,7 +90,7 @@ def main():
     for source in sources:
         tractor.addSources(source)
 
-    zr = np.array([-5.,+5.]) * info['skysig']
+    zr = info['zr']
 
     print bands
 

tractor/sdss.py
@@ -425,6 +425,10 @@ def get_tractor_image(run, camcol, field, bandname,
 			   np.clip(yc-S, 0, H),
 			   np.clip(yc+S, 0, H)]
 
+		if roi[0]==roi[1] or roi[2]==roi[3]:
+			print "ZERO ROI?", roi
+			return None,None
+
 		info.update(roi=roi)
 		
 	if roi is not None:
@@ -443,7 +447,8 @@ def get_tractor_image(run, camcol, field, bandname,
 	sky = psfield.getSky(bandnum)
 	skysig = sqrt(sky)
 	skyobj = ConstantSky(sky)
-	info.update(sky=sky, skysig=skysig)
+	zr = np.array([-5.,+5.]) * skysig
+	info.update(sky=sky, skysig=skysig,zr=zr)
 
 	fpM = sdss.readFpM(run, camcol, field, bandname)
 	gain = psfield.getGain(bandnum)