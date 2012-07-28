77dcb60d69c3bc0a7217cf6134059964e7942dbe
Ekta Patel <ep1091@physics.nyu.edu>
2012-07-28 20:08:50 +0000

    update
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@21187 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- allRC3.py
+++ allRC3.py
@@ -2,18 +2,37 @@ import os
 import numpy as np
 import pylab as plt
 import pyfits
+import sys
 
 #Work in progress...
 def main():
     rc3 = pyfits.open('rc3limited.fits')
+    entries=[]
     for entry in rc3[1].data:
+        print (10**entry['LOG_D25'])/10.
         fn = '%s.pickle' % (entry['NAME'])
         if os.path.exists(fn):
-            continue
+            continue 
+            print '%s has run successfully already' %entry['NAME'] 
         else:
             pass
+            print 'run %s through tractor' %entry['NAME']
+            entries.append('%s' %entry['NAME'])
+    things=[str(x) for x in entries]
+    print things
+    #print entries
+    
+    for entry in things:
+        print 'entry',
+        os.system("python -u general.py '%s' --threads 4 --itune1 6 --itune2 6 --nocache 1>%s.log 2>%s_err.log" % (entry,entry,entry))
+        print 'running tractor for %s' %entry
+        os.system('cp flip-%s.pdf RC3_Output' % entry)
+        os.system('cp ngc-%s.png RC3_Output' % entry)
+        os.system('cp flip-%s.tex RC3_Output' % entry)
+        
+            
+#some rc3 entries dont have a name in the 'NAME' data field. what to do with those? Also, the space in the name NGC 1234 is causing problems with the os.system commands. run this file and you will see what i am talking about. thats all that needs to be fixed and everything else should be good to go
 
-#dm-shouldnt this be the other way around? if the pickle file exists then pass but if not then continue on to 'general.py'?
-
+#3053 galaxies
 if __name__ == '__main__':
     main()
