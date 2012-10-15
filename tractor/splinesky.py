db260cc74e43b0e43beee6f5c4dbb3618773d1d2
Dustin Lang <dstndstn@gmail.com>
2012-10-15 18:43:10 +0000

    plot knot positions and non-zero regions
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@21531 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- tractor/splinesky.py
+++ tractor/splinesky.py
@@ -2,13 +2,12 @@ import numpy as np
 import scipy.interpolate as interp
 from utils import *
 
-#class SplineSky(NamedParams):
 class SplineSky(ParamList):
 	def __init__(self, X, Y, bg):
 		## Note -- using weights, we *could* try to make the spline
 		# constructor fit for the sky residuals (averaged over the 
 		# spline boxes)
-		
+
 		self.spl = interp.RectBivariateSpline(X, Y, bg.T)
 		print 'fp', self.spl.fp
 		print 'tck', self.spl.tck
@@ -20,23 +19,8 @@ class SplineSky(ParamList):
 		print 'c', c
 		print ','.join(['%g'%x for x in tx])
 		super(SplineSky, self).__init__(*c)
-		# override
+		# override -- "c" is a list, so this should work as expected
 		self.vals = c
-	# def _getc(self):
-	# 	tck = self.spl.tck
-	# 	(tx,ty,c) = tck
-	# 	return c
-	# def _numberOfThings(self):
-	# 	return len(self._getc())
-	# def _getThing(self, i):
-	# 	return self._getc()[i]
-	# def _getThings(self):
-	# 	return self._getc()
-	# def _setThing(self, i, v):
-	# 	self._getc()[i] = v
-
-	#def getParamDerivatives(self, img):
-	#	pass
 
 	def addTo(self, mod):
 		H,W = mod.shape
@@ -75,9 +59,28 @@ if __name__ == '__main__':
 		ax = plt.axis()
 		plt.colorbar()
 		for x in tx:
-			plt.axvline(x, '-', color='k')
+			plt.axhline(x, color='0.5')
 		for y in ty:
-			plt.axhline(x, '-', color='k')
+			plt.axvline(y, color='0.5')
+
+		H,W = X.shape
+		# Find non-zero range
+		for nzx0 in range(W):
+			if not np.all(X[:,nzx0] == 0):
+				break
+		for nzx1 in range(W-1,-1,-1):
+			if not np.all(X[:,nzx1] == 0):
+				break
+		for nzy0 in range(H):
+			if not np.all(X[nzy0,:] == 0):
+				break
+		for nzy1 in range(H-1,-1,-1):
+			if not np.all(X[nzy1,:] == 0):
+				break
+		plt.axhline(nzy0, color='y')
+		plt.axhline(nzy1, color='y')
+		plt.axvline(nzx0, color='y')
+		plt.axvline(nzx1, color='y')
 		plt.axis(ax)
 			
 	plotsky(X)
