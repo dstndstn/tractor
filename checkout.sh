4aef22fa8d8ecab346afeb77ea796116c25bb04f
Dustin Lang <dstndstn@gmail.com>
2013-01-24 18:36:06 +0000

    don't checkout tractor/ into subdirectory
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@22056 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- checkout.sh
+++ checkout.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 set -e
+
 mkdir tractor
 cd tractor
-svn co http://astrometry.net/svn/trunk/projects/tractor
+
+svn co http://astrometry.net/svn/trunk/projects/tractor .
 svn co -N http://astrometry.net/svn/trunk/src/astrometry
 (cd astrometry && svn up util)
 (cd astrometry && svn up libkd)
@@ -11,5 +13,5 @@ svn co -N http://astrometry.net/svn/trunk/src/astrometry
 (cd astrometry && svn up sdss)
 
 (cd astrometry && make pyutil)
-(cd tractor && make)
+make
 
