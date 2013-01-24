7b68830711a77cbc834f7737d435156a0ae92510
Dustin Lang <dstndstn@gmail.com>
2013-01-24 18:04:08 +0000

    add checkout script
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@22049 03a3cea6-2c03-0410-ac9b-9271b1c66c29

---
+++ checkout.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+set -e
+mkdir tractor
+cd tractor
+svn co http://astrometry.net/svn/trunk/projects/tractor
+svn co -N http://astrometry.net/svn/trunk/src/astrometry
+(cd astrometry && svn up util)
+(cd astrometry && svn up libkd)
+(cd astrometry && svn up qfits-an)
+(cd astrometry && svn up gsl-an)
+
+(cd astrometry && make pyutil)
+(cd tractor && make)
