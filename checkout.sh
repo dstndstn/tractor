d03d004b9164fe84539965ad6ea2d214ea786b26
Dustin Lang <dstndstn@gmail.com>
2013-01-24 18:25:16 +0000

    need sdss too!
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@22053 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- checkout.sh
+++ checkout.sh
@@ -8,6 +8,8 @@ svn co -N http://astrometry.net/svn/trunk/src/astrometry
 (cd astrometry && svn up libkd)
 (cd astrometry && svn up qfits-an)
 (cd astrometry && svn up gsl-an)
+(cd astrometry && svn up sdss)
 
 (cd astrometry && make pyutil)
 (cd tractor && make)
+
