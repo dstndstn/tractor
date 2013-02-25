7195a10d0f31dc864ea105a389b0dc8e811e8029
Dustin Lang <dstndstn@gmail.com>
2013-02-25 21:42:46 +0000

    plot results
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@22307 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- cs82b.py
+++ cs82b.py
@@ -12,7 +12,7 @@ from astrometry.util.pyfits_utils import *
 from astrometry.util.sdss_radec_to_rcf import *
 from astrometry.util.multiproc import *
 from astrometry.util.file import *
-from astrometry.util.plotutils import ArcsinhNormalize
+from astrometry.util.plotutils import *
 from astrometry.util.util import *
 from astrometry.sdss import *
 from tractor import *
@@ -354,6 +354,11 @@ def main(opt):
 		F = fits_table(fn)
 	else:
 		F = fits_table('window_flist-DR9.fits')
+
+		# These run doesn't appear in DAS
+		#F.cut((F.run != 4322) * (F.run != 4240) * (F.run != 4266))
+		F.cut(F.rerun != "157")
+
 		# For Stripe 82, mu-nu is aligned with RA,Dec.
 		rd = []
 		rd.append(munu_to_radec_deg(F.mu_start, F.nu_start, F.node, F.incl))
@@ -379,35 +384,159 @@ def main(opt):
 		F.writeto(fn)
 		print 'Wrote', fn
 
-
-	if opt.cmd:
-
-		print len(np.unique(F.run)), 'unique runs'
-		
-		for r,c,f,r0,r1,d0,d1 in zip(F.run, F.camcol, F.field,
-									 F.ra0, F.ra1, F.dec0, F.dec1):
-			for band in 'ugriz':
-				basename = 'cs82-%s-%04i-%i-%04i-%s' % (cs82field, r, c, f, band)
-				fn = 'mags-%s.fits' % basename
-				M = fits_table(fn)
-				
-
-
-			
-
-		return
-
-
-
-		
-	mp = multiproc(opt.threads)
-
 	# Sort by distance from the center of the field.
 	ra  = (T.ra.min()  + T.ra.max() ) / 2.
 	dec = (T.dec.min() + T.dec.max()) / 2.
 	I = np.argsort( ((F.ra0  + F.ra1 )/2. - ra )**2 +
 					((F.dec0 + F.dec1)/2. - dec)**2 )
 	F.cut(I)
+
+
+
+	if opt.cmd:
+
+		urun = np.unique(F.run)
+		print len(urun), 'unique runs'
+
+		rmap = dict([(r,i) for i,r in enumerate(urun)])
+
+		for band in 'ugriz':
+			T.set('all_%s' % band, np.zeros((len(T), len(urun)), np.float32))
+
+		for r,c,f,r0,r1,d0,d1 in zip(F.run, F.camcol, F.field,
+									 F.ra0, F.ra1, F.dec0, F.dec1):
+			ri = rmap[r]
+			print 'Run', r, '->', ri
+			for band in 'ugriz':
+				basename = 'cs82-%s-%04i-%i-%04i-%s' % (cs82field, r, c, f, band)
+				fn = 'mags-%s.fits' % basename
+				if not os.path.exists(fn):
+					print 'No such file', fn, '-- skipping'
+					continue
+				print 'Reading', fn
+				M = fits_table(fn)
+				print 'Got', len(M)
+				m = M.get('sdss_mag_%s' % band)
+				m[np.logical_not(np.isfinite(m))] = 0.
+				I = M.cs82_index
+				T.get('all_%s' % band)[I, ri] = m
+
+		T.mag = np.zeros_like(T.mag_psf)
+		T.mag[T.chi2_psf < T.chi2_model] = T.mag_psf
+		J = T.chi2_psf >= T.chi2_model
+		T.mag[J] = NanoMaggies.nanomaggiesToMag(
+			NanoMaggies.magToNanomaggies(T.mag_disk[J]) +
+			NanoMaggies.magToNanomaggies(T.mag_spheroid[J]))
+
+		for band in 'ugriz':
+			smag = T.get('all_%s' % band)
+			
+			mn = []
+			st = []
+			cc = []
+			ss = []
+			V = []
+			for i in range(len(T)):
+				J = np.flatnonzero(smag[i,:])
+				if len(J) == 0:
+					continue
+				V.append(i)
+				s = smag[i,J]
+				ss.append(s)
+				c = np.zeros_like(s) + T.mag[i]
+				cc.append(c)
+				mn.append(np.mean(s))
+				st.append(np.std(s))
+			V = np.array(V)
+			Ti = T[V]
+			J = (Ti.chi2_psf < Ti.chi2_model)
+			plt.clf()
+			plt.plot(np.hstack([c for c,s,psf in zip(cc,ss,J) if psf]),
+					 np.hstack([s for c,s,psf in zip(cc,ss,J) if psf]), 'b.', alpha=0.1, zorder=20)
+			plt.plot(np.hstack([c for c,s,psf in zip(cc,ss,J) if not psf]),
+					 np.hstack([s for c,s,psf in zip(cc,ss,J) if not psf]), 'b.', alpha=0.1, zorder=20)
+			mn = np.array(mn)
+			st = np.array(st)
+			plt.errorbar(Ti.mag[J], mn[J], yerr=st[J], fmt='.', color='r', alpha=0.5, zorder=25)
+			K = np.logical_not(J)
+			plt.errorbar(Ti.mag[K], mn[K], yerr=st[K], fmt='.', color='b', alpha=0.5, zorder=25)
+			plt.xlim(26, 8)
+			plt.ylim(26, 8)
+			plt.savefig('mm-%s.png' % band)
+
+			plt.clf()
+			plt.plot(np.hstack([c - s for c,s,psf in zip(cc,ss,J) if psf]),
+					 np.hstack([c for c,s,psf in zip(cc,ss,J) if psf]), 'b.', alpha=0.1, zorder=20)
+			plt.errorbar(Ti.mag[J] - mn[J], Ti.mag[J], xerr=st[J], fmt='.', color='b', alpha=0.5, zorder=25)
+			plt.ylim(26, 8)
+			plt.xlim(-10,10)
+			plt.xlabel('CFHT i - SDSS %s (mag)' % band)
+			plt.ylabel('CFHT i (mag)')
+			plt.savefig('cm-stars-%s.png' % band)
+			
+			plt.clf()
+			plt.plot(np.hstack([c - s for c,s,psf in zip(cc,ss,J) if not psf]),
+					 np.hstack([c for c,s,psf in zip(cc,ss,J) if not psf]), 'r.', alpha=0.1, zorder=20)
+			plt.errorbar(Ti.mag[K] - mn[K], Ti.mag[K], xerr=st[K], fmt='.', color='r', alpha=0.5, zorder=25)
+			plt.ylim(26, 8)
+			plt.xlim(-10,10)
+			plt.xlabel('CFHT i - SDSS %s (mag)' % band)
+			plt.ylabel('CFHT i (mag)')
+			plt.savefig('cm-gals-%s.png' % band)
+
+			plt.clf()
+			plt.plot(np.hstack([c - s for c,s,psf in zip(cc,ss,J) if not psf]),
+					 np.hstack([c for c,s,psf in zip(cc,ss,J) if not psf]), 'r.', alpha=0.1, zorder=20)
+			plt.ylim(26, 8)
+			plt.xlim(-10,10)
+			plt.xlabel('CFHT i - SDSS %s (mag)' % band)
+			plt.ylabel('CFHT i (mag)')
+			plt.savefig('cm-gals-%s-1.png' % band)
+
+			plt.clf()
+			plt.errorbar(Ti.mag[K] - mn[K], Ti.mag[K], xerr=st[K], fmt='.', color='r', alpha=0.5, zorder=25)
+			plt.ylim(26, 8)
+			plt.xlim(-10,10)
+			plt.xlabel('CFHT i - SDSS %s (mag)' % band)
+			plt.ylabel('CFHT i (mag)')
+			plt.savefig('cm-gals-%s-2.png' % band)
+
+			plt.clf()
+			loghist(Ti.mag[K] - mn[K], Ti.mag[K], bins=200, range=((-10,10),(8,26)))
+			plt.ylim(26, 8)
+			plt.savefig('cm-gals-%s-3.png' % band)
+
+			plt.clf()
+			loghist(Ti.mag[J] - mn[J], Ti.mag[J], bins=200, range=((-10,10),(8,26)))
+			plt.ylim(26, 8)
+			plt.savefig('cm-stars-%s-3.png' % band)
+
+
+			xl,xh = -5,3
+			yl,yh = 16,24
+			
+			plt.clf()
+			loghist(Ti.mag[K] - mn[K], Ti.mag[K], bins=200, range=((xl,xh),(yl,yh)))
+			plt.ylim(yh,yl)
+			plt.xlabel('CFHT i - SDSS %s (mag)' % band)
+			plt.ylabel('CFHT i (mag)')
+			plt.savefig('cm-gals-%s-4.png' % band)
+
+			plt.clf()
+			loghist(Ti.mag[J] - mn[J], Ti.mag[J], bins=200, range=((xl,xh),(yl,yh)))
+			plt.ylim(yh,yl)
+			plt.xlabel('CFHT i - SDSS %s (mag)' % band)
+			plt.ylabel('CFHT i (mag)')
+			plt.savefig('cm-stars-%s-4.png' % band)
+
+
+				
+		return
+
+
+
+		
+	mp = multiproc(opt.threads)
 
 	results = []
 	args = []
@@ -471,6 +600,9 @@ if __name__ == '__main__':
 					  help='Skip fields whose outputs already exist?')
 	parser.add_option('-n', dest='nfields', type=int, default=None,
 					  help='Run at most this number of fields')
+
+	parser.add_option('-S', dest='simul', action='store_true',
+					  help='Simultaneous fit?')
 
 	parser.add_option('--cmd', action='store_true',
 					  help='Produce summary CMD plots')
