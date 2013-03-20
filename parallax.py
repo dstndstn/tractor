434344d9400efef92e14843b5a154a0a96f7d4b0
Dustin Lang <dstndstn@gmail.com>
2013-03-20 21:09:09 +0000

    add optparse
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@22442 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- parallax.py
+++ parallax.py
@@ -308,250 +308,288 @@ def plot_images(tr, ptype='mod', mods=None):
 					  fontsize=8)
 
 	return dict(rows=rows, cols=cols)
-		
 
-if __name__ == '__main__':
-	plot_chain('parallax-star0-z-step1000.pickle', PlotSequence('chain-0z'),
-			   52.646659, -0.42659916, 'z', 0)
-	plot_chain('parallax-star1-z-step1000.pickle', PlotSequence('chain-1z'),
-			   314.48301, -0.83521203, 'z', 1)
-	sys.exit(0)
-	pass
 
-plt.figure(figsize=(10,10))
-	
-window_flist_fn = 'window_flist-DR9.fits'
-T = fits_table(window_flist_fn)
-print 'Read', len(T), 'S82 fields'
+class SourceInfo(object):
+	def __init__(self, **kwargs):
+		for k,v in kwargs.items():
+			self.setattr(k, v)
 
-T.cut(T.score >= 0.5)
-print 'Cut to', len(T), 'photometric'
-
-sdss = DR9(basedir='data-dr9')
-
-ps = PlotSequence('parallax')
-
-for stari,(ra,dec, pmra,pmdec,parallax) in enumerate(
+def getSource(i):
+	srcs = []
+	for stari,(ra,dec, pmra,pmdec,parallax) in enumerate(
 		[(52.646659, -0.42659916, 414., -355., 40.),
-		 (314.48301, -0.83521203, -37., -30., 33.),]):
+		(314.48301, -0.83521203, -37., -30., 33.),]):
+		srcs.append(SourceInfo(ra=ra, dec=dec, pmra=pmra, pmdec=pmdec,
+							   parallax=parallax, srci=stari))
+	return srcs[i]
+
+def run_star(src, band, opt):
+	window_flist_fn = 'window_flist-DR9.fits'
+	T = fits_table(window_flist_fn)
+	print 'Read', len(T), 'S82 fields'
+	T.cut(T.score >= 0.5)
+	print 'Cut to', len(T), 'photometric'
+
+	sdss = DR9(basedir='data-dr9')
+
+	stari = src.srci
+	ps = PlotSequence('parallax-%i-%s' % (stari, band))
+
+	ra = src.ra
+	dec = src.dec
+	pmra = src.pmra
+	pmdec = src.pmdec
+	parallax = src.parallax
+
 	I = np.flatnonzero(np.hypot(T.ra - ra, T.dec - dec) < np.hypot(13.,9.)/(2.*60.))
 	print 'Got', len(I), 'fields possibly in range'
-
 	print 'Runs:', np.unique(T.run[I])
-
 	S = 15
 
-	#for band in 'ugriz':
-	for band in 'z':
+	# We use this to estimate the source position within each image
+	# when deciding whether to keep or not.
+	tmid = TAITime(4.55759e+09)
 
-		tmid = TAITime(4.55759e+09)
-
-		nm = NanoMaggies.magToNanomaggies(20.)
-		pm = PMRaDec(pmra / (1000.*3600.), pmdec / (1000.*3600.))
-		print 'Proper motion:', pm
-		mps = MovingPointSource(RaDecPos(ra, dec), NanoMaggies(**{band:nm}),
-								pm, parallax / 1000., epoch=tmid)
-		print 'source:', mps
+	nm = NanoMaggies.magToNanomaggies(20.)
+	pm = PMRaDec(pmra / (1000.*3600.), pmdec / (1000.*3600.))
+	#print 'Proper motion:', pm
+	mps = MovingPointSource(RaDecPos(ra, dec), NanoMaggies(**{band:nm}),
+							pm, parallax / 1000., epoch=tmid)
+	#print 'source:', mps
 		
-		tims = []
-		for ii in I:
-			t = T[ii]
+	tims = []
+	for ii in I:
+		t = T[ii]
+		print
+		print 'R/C/F', t.run, t.camcol, t.field
 
-			print 'R/C/F', t.run, t.camcol, t.field
+		tim,tinf = get_tractor_image_dr8(
+			t.run, t.camcol, t.field, band,
+			sdss=sdss, roiradecsize=(ra,dec,S),
+			nanomaggies=True, invvarIgnoresSourceFlux=True)
+		if tim is None:
+			continue
+		tim.score = t.score
 
-			tim,tinf = get_tractor_image_dr8(
-				t.run, t.camcol, t.field, band,
-				sdss=sdss, roiradecsize=(ra,dec,S),
-				nanomaggies=True, invvarIgnoresSourceFlux=True)
-			#zrange=[-2,5])
-			if tim is None:
-				continue
-			tim.score = t.score
-			#tim.tai = tinf['tai']
+		pos = mps.getPositionAtTime(tim.time)
+		x,y = tim.getWcs().positionToPixel(pos)
+		#print 'x,y', x,y
+		H,W = tim.shape
+		#print 'W,H', W,H
+		#print 'roi', tinf['roi']
+		if x < 0 or y < 0 or x >= W or y >= H:
+			print 'Skipping -- OOB'
+			continue
 
-			pos = mps.getPositionAtTime(tim.time)
-			x,y = tim.getWcs().positionToPixel(pos)
-			print 'x,y', x,y
-			H,W = tim.shape
-			print 'W,H', W,H
-
-			print 'roi', tinf['roi']
+		roi = tinf['roi']
+		ymid = (roi[2]+roi[3]) / 2.
+		if ymid < 64. or ymid > 1425.:
+			print 'Duplicate field -- ymid=%f' % ymid
+			continue
 			
-			if x < 0 or y < 0 or x >= W or y >= H:
-				print 'Skipping -- OOB'
-				continue
+		tim.rcf = (t.run, t.camcol, t.field)
+		tims.append(tim)
 
-			roi = tinf['roi']
-			ymid = (roi[2]+roi[3]) / 2.
-			if ymid < 64. or ymid > 1425.:
-				print 'Duplicate field -- ymid=%f' % ymid
-				continue
-			
-			tim.rcf = (t.run, t.camcol, t.field)
+	# print 'times', [tim.time for tim in tims]
+	t0 = min([tim.time for tim in tims])
+	print 't0:', t0
+	t1 = max([tim.time for tim in tims])
+	print 't1:', t1
+	tmid = (t0 + t1)/2.
+	print 'tmid:', tmid
 
-			tims.append(tim)
+	# Re-create this object now that we have the real "tmid".
+	mps = MovingPointSource(RaDecPos(ra, dec), NanoMaggies(**{band:nm}),
+							pm, parallax / 1000., epoch=tmid)
+	tr = Tractor(tims, [mps])
+	tr.freezeParam('images')
+	print 'Tractor:', tr
 
-		# print 'times', [tim.time for tim in tims]
-		t0 = min([tim.time for tim in tims])
-		print 't0:', t0
-		t1 = max([tim.time for tim in tims])
-		print 't1:', t1
-		tmid = (t0 + t1)/2.
-		print 'tmid:', tmid
+	PA = plot_images(tr, ptype='img')
+	plt.suptitle('%s band: data' % (band))
+	ps.savefig()
 
+	rows = PA['rows']
+	cols = PA['cols']
 
-		tr = Tractor(tims, [mps])
-		tr.freezeParam('images')
-		print 'Tractor:', tr
-
-		plot_images(tr, ptype='img')
-		plt.suptitle('%s band: data' % (band))
+	# Plot derivatives
+	DD = [mps.getParamDerivatives(tim) for tim in tims]
+	for i,nm in enumerate(mps.getParamNames()):
+		plt.clf()
+		for j,tim in enumerate(tims):
+			D = DD[j][i]
+			dd = np.zeros_like(tim.getImage())
+			D.addTo(dd)
+			mx = max(dd.max(), np.abs(dd.min()))
+			plt.subplot(rows, cols, j+1)
+			plt.imshow(dd, interpolation='nearest', origin='lower',
+					   vmin=-mx, vmax=mx)
+			plt.gray()
+			plt.xticks([]); plt.yticks([])
+			plt.title('%.2f yr' % ((tim.time - t0).toYears()))
+		plt.suptitle('%s band: derivs for param: %s' % (band, nm))
 		ps.savefig()
 
-		N = len(tims)
-		cols = int(np.ceil(np.sqrt(N)))
-		rows = int(np.ceil(N / float(cols)))
+	plot_images(tr, ptype='mod')
+	plt.suptitle('%s band: initial model' % band)
+	ps.savefig()
 
-		DD = [mps.getParamDerivatives(tim) for tim in tims]
-		for i,nm in enumerate(mps.getParamNames()):
+	plot_images(tr, ptype='chi')
+	plt.suptitle('%s band: initial chi' % band)
+	ps.savefig()
+
+	while True:
+		dlnp,X,alpha = tr.optimize()
+		print 'Stepping by', alpha, 'for dlnp', dlnp
+		if alpha == 0:
+			break
+		if dlnp < 1e-3:
+			break
+
+	print 'opt source:', mps
+
+	plot_images(tr, ptype='mod')
+	plt.suptitle('%s band: opt model' % band)
+	ps.savefig()
+
+	plot_images(tr, ptype='chi')
+	plt.suptitle('%s band: opt chi' % band)
+	ps.savefig()
+
+	p0 = np.array(tr.getParams())
+	ndim = len(p0)
+	nw = 50
+	print 'ndim', ndim
+	print 'nw', nw
+
+	sampler = emcee.EnsembleSampler(nw, ndim, tr, threads=8)
+	
+	steps = np.array(tr.getStepSizes())
+	colscales = tr.getParameterScales()
+
+	pp0 = np.vstack([p0 + 1e-4 * steps / colscales *
+					 np.random.normal(size=len(steps))
+					 for i in range(nw)])
+	
+	alllnp = []
+	allp = []
+
+	lnp = None
+	pp = pp0
+	rstate = None
+	for step in range(1001):
+		print 'Taking emcee step', step
+		pp,lnp,rstate = sampler.run_mcmc(pp, 1, lnprob0=lnp, rstate0=rstate)
+		#print 'lnprobs:', lnp
+
+		if step and (step % 50 == 0):
+			mods = [np.zeros_like(tim.getImage()) for tim in tims]
+			for k,(p,x) in enumerate(zip(lnp,pp)):
+				tr.setParams(x)
+				for j,tim in enumerate(tims):
+					mods[j] += tr.getModelImage(tim)
+			for mod in mods:
+				mod /= len(lnp)
+
+			plot_images(tr, ptype='mod', mods=mods)
+			plt.suptitle('%s band: sample %i, sum' % (band, step))
+			ps.savefig()
+			
+			plot_images(tr, ptype='chi', mods=mods)
+			plt.suptitle('%s band: sample %i, sum' % (band, step))
+			ps.savefig()
+				
+			#print 'pp', pp.shape
+			
 			plt.clf()
-			for j,tim in enumerate(tims):
-				D = DD[j][i]
-				dd = np.zeros_like(tim.getImage())
-				D.addTo(dd)
-				mx = max(dd.max(), np.abs(dd.min()))
-				plt.subplot(rows, cols, j+1)
-				plt.imshow(dd, interpolation='nearest', origin='lower',
-						   vmin=-mx, vmax=mx)
-				plt.gray()
-				plt.xticks([]); plt.yticks([])
-				plt.title('%.2f yr' % ((tim.time - t0).toYears()))
-			plt.suptitle('%s band: derivs for param: %s' % (band, nm))
+			nparams = ndim
+			nm = [n.split('.')[-1] for n in mps.getParamNames()]
+			k = 1
+			for i in range(nparams):
+				for j in range(nparams):
+					plt.subplot(nparams, nparams, k)
+					if i == j:
+						plt.hist(pp[:,i], 20)
+					else:
+						plt.plot(pp[:,j], pp[:,i], 'b.')
+
+					if k % nparams == 1:
+						plt.ylabel(nm[i])
+					if (k-1) / nparams == (nparams - 1):
+						plt.xlabel(nm[j])
+					plt.xticks([])
+					plt.yticks([])
+					k += 1
+			plt.suptitle('%s band: sample %i' % (band, step))
 			ps.savefig()
 
-		plot_images(tr, ptype='mod')
-		plt.suptitle('%s band: initial model' % band)
-		ps.savefig()
-
-		plot_images(tr, ptype='chi')
-		plt.suptitle('%s band: initial chi' % band)
-		ps.savefig()
-
-		while True:
-			dlnp,X,alpha = tr.optimize()
-			print 'Stepping by', alpha, 'for dlnp', dlnp
-			if alpha == 0:
-				break
-			if dlnp < 1e-3:
-				break
-
-		print 'opt source:', mps
-
-		plot_images(tr, ptype='mod')
-		plt.suptitle('%s band: opt model' % band)
-		ps.savefig()
-
-		plot_images(tr, ptype='chi')
-		plt.suptitle('%s band: opt chi' % band)
-		ps.savefig()
-
-		p0 = np.array(tr.getParams())
-		ndim = len(p0)
-		nw = 50
-		print 'ndim', ndim
-		print 'nw', nw
-
-		sampler = emcee.EnsembleSampler(nw, ndim, tr, threads=8)
-		
-		steps = np.array(tr.getStepSizes())
-		colscales = tr.getParameterScales()
-
-		pp0 = np.vstack([p0 + 1e-4 * steps / colscales *
-						 np.random.normal(size=len(steps))
-						 for i in range(nw)])
-		
-		alllnp = []
-		allp = []
-
-		lnp = None
-		pp = pp0
-		rstate = None
-		for step in range(1001):
-			print 'Taking emcee step', step
-			pp,lnp,rstate = sampler.run_mcmc(pp, 1, lnprob0=lnp, rstate0=rstate)
-			#print 'lnprobs:', lnp
-
-			if step and (step % 50 == 0):
-				mods = [np.zeros_like(tim.getImage()) for tim in tims]
-				for k,(p,x) in enumerate(zip(lnp,pp)):
-					tr.setParams(x)
-					for j,tim in enumerate(tims):
-						mods[j] += tr.getModelImage(tim)
-				for mod in mods:
-					mod /= len(lnp)
-
-				plot_images(tr, ptype='mod', mods=mods)
-				plt.suptitle('%s band: sample %i, sum' % (band, step))
-				ps.savefig()
-				
-				plot_images(tr, ptype='chi', mods=mods)
-				plt.suptitle('%s band: sample %i, sum' % (band, step))
-				ps.savefig()
-					
-				print 'pp', pp.shape
-				
-				plt.clf()
-				nparams = ndim
-				nm = [n.split('.')[-1] for n in mps.getParamNames()]
-				k = 1
-				for i in range(nparams):
-					for j in range(nparams):
-						plt.subplot(nparams, nparams, k)
-						if i == j:
-							plt.hist(pp[:,i], 20)
-						else:
-							plt.plot(pp[:,j], pp[:,i], 'b.')
-
-						if k % nparams == 1:
-							#print 'k', k, 'i', i, 'j', j, '-> ylabel', nm[i]
-							plt.ylabel(nm[i])
-
-						if (k-1) / nparams == (nparams - 1):
-							plt.xlabel(nm[j])
-							#print 'k', k, 'i', i, 'j', j, '-> xlabel', nm[j]
-							#else:
-						plt.xticks([])
-						plt.yticks([])
-						k += 1
-				plt.suptitle('%s band: sample %i' % (band, step))
-				ps.savefig()
-
-				plt.clf()
-				for i in range(nparams + 1):
-					if i == 0:
-						p = np.array(alllnp)
-						ll = 'lnp'
-					else:
-						p = np.array(allp)[:,:,i-1]
-						ll = nm[i-1]
-					plt.subplot(nparams+1, 1, i+1)
-					N,W = p.shape
-					for j in range(W):
-						plt.plot(p[:,j], 'k-', alpha=0.2)
-					plt.xlabel(ll)
-				ps.savefig()
-				
-			print 'Max lnprob:', max(lnp)
-			print 'Std in lnprobs:', np.std(lnp)
-
-			alllnp.append(lnp.copy())
-			allp.append(pp.copy())
-
-			if (step+0) % 100 == 0:
-				pickle_to_file(dict(alllnp=alllnp, allp=allp, tr=tr),
-							   'parallax-star%i-%s-step%04i.pickle' % (stari, band, step))
+			plt.clf()
+			for i in range(nparams + 1):
+				if i == 0:
+					p = np.array(alllnp)
+					ll = 'lnp'
+				else:
+					p = np.array(allp)[:,:,i-1]
+					ll = nm[i-1]
+				plt.subplot(nparams+1, 1, i+1)
+				N,W = p.shape
+				for j in range(W):
+					plt.plot(p[:,j], 'k-', alpha=0.2)
+				plt.xlabel(ll)
+			ps.savefig()
 			
-		pickle_to_file(dict(alllnp=alllnp, allp=allp, tr=tr),
-					   'parallax-star%i-%s-end.pickle' % (stari, band))
+		print 'Max lnprob:', max(lnp)
+		print 'Std in lnprobs:', np.std(lnp)
+
+		alllnp.append(lnp.copy())
+		allp.append(pp.copy())
+
+		if (step+0) % 100 == 0:
+			pickle_to_file(dict(alllnp=alllnp, allp=allp, tr=tr),
+						   'parallax-star%i-%s-step%04i.pickle' % (stari, band, step))
+		
+	pickle_to_file(dict(alllnp=alllnp, allp=allp, tr=tr),
+				   'parallax-star%i-%s-end.pickle' % (stari, band))
 
 			
+
+
+if __name__ == '__main__':
+	from optparse import OptionParser
+	import sys
+	parser = OptionParser(usage='%prog [options]')
+	parser.add_option('-p', '--plots', dest='plots', action='store_true',
+					  help='Make summary plots?')
+	parser.add_option('-s', '--source', dest='sources', action='append', type=int,
+					  help='Operate on the given (set of) sources')
+	parser.add_option('-P', dest='parallax', action='store_false', default=True,
+					  help='Freeze the parallax at zero?')
+	parser.add_option('-b', dest='band', type=str, default='z', help='Band (default %default)')
+	parser.add_option('-v', '--verbose', dest='verbose', action='count', default=0,
+					  help='Make more verbose')
+
+	opt,args = parser.parse_args()
+	if opt.verbose == 0:
+		lvl = logging.INFO
+	else:
+		lvl = logging.DEBUG
+	logging.basicConfig(level=lvl, format='%(message)s', stream=sys.stdout)
+
+	plt.figure(figsize=(10,10))
+
+	srcs = [getSource(i) for i in opt.sources]
+	
+	if opt.plots:
+		band = 'z'
+		for src in srcs:
+			plot_chain('parallax-star%i-%s-end.pickle' % (src.srci, band),
+					   PlotSequence('chain-%i%s' % (src.srci, band)),
+					   src.ra, src.dec, band, src.srci)
+		sys.exit(0)
+
+	for src in srcs:
+		run_star(src, opt.band, opt)
+		
+
+	
