15f3ec81e792065cad07e368cd4524762a4208f5
Dustin Lang <dstndstn@gmail.com>
2013-04-11 20:00:57 +0000

    video of WISE frames being accumulated
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@22572 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- wise2.py
+++ wise2.py
@@ -28,6 +28,12 @@ from tractor.fitpsf import em_init_params
 
 from wise import *
 
+def get_l1b_file(scanid, frame, band):
+	assert(band == 1)
+	scangrp = scanid[-2:]
+	return os.path.join('/clusterfs/riemann/raid007/bosswork/boss/wise_level1b/wise1/4band_p1bm_frm',
+						scangrp, scanid, '%03i' % frame, '%s%03i-w1-int-1b.fits' % (scanid, frame))
+
 
 if __name__ == '__main__':
 
@@ -37,10 +43,10 @@ if __name__ == '__main__':
 	#dechi = 1.25
 	#width = 7
 
-	ralo = 38.5
-	rahi = 40.5
-	declo = -0.5
-	dechi = 1.5
+	ralo = 38.25
+	rahi = 40.75
+	declo = -0.75
+	dechi = 1.75
 	width = 2.5
 
 	rl,rh = 39,40
@@ -51,6 +57,8 @@ if __name__ == '__main__':
 
 	basedir = '/project/projectdirs/bigboss'
 	wisedatadir = os.path.join(basedir, 'data', 'wise')
+
+	wisedatadir = '/clusterfs/riemann/raid007/bosswork/boss/wise_level1b'
 	
 	ifn = os.path.join(wisedatadir, 'index-allsky-astr-L1b.fits')
 	T = fits_table(ifn, columns=['ra','dec','scan_id','frame_num'])
@@ -63,8 +71,6 @@ if __name__ == '__main__':
 	I = np.flatnonzero((T.ra > ralo) * (T.ra < rahi) * (T.dec > declo) * (T.dec < dechi))
 	print len(I), 'overlap RA,Dec box'
 
-	for sid,fnum in zip(T.scan_id[I], T.frame_num[I]):
-		print 'scan,frame', sid, fnum
 
 	T = fits_table(ifn, rows=I)
 	print 'Read', len(T), 'rows'
@@ -77,6 +83,8 @@ if __name__ == '__main__':
 	wcses = []
 	corners = []
 
+	ii = []
+	
 	for i in range(len(T)):
 		hdr = T.header[i]
 		hdr = [str(s) for s in hdr]
@@ -89,12 +97,10 @@ if __name__ == '__main__':
 		hdrstr = ''.join(hdr)
 		newhdr.append(hdrstr)
 
-		print hdrstr
+		#print hdrstr
 
 		wcs = getWcsFromHeaderString(hdrstr)
-
-		W,H = 1016,1016
-
+		W,H = wcs.get_width(), wcs.get_height()
 		rd = []
 		for x,y in [(1,1),(1,H),(W,H),(W,1)]:
 			rd.append(wcs.pixelxy2radec(x,y))
@@ -103,6 +109,11 @@ if __name__ == '__main__':
 		
 		if polygons_intersect(roipoly, rd):
 			wcses.append(wcs)
+			ii.append(i)
+
+	print 'Found', len(wcses), 'overlapping'
+	I = np.array(ii)
+	T.cut(I)
 
 	corners = np.vstack(corners)
 	print 'Corners', corners.shape
@@ -128,3 +139,32 @@ if __name__ == '__main__':
 	plot.alpha = 1.0
 	plot.plot_grid(1, 1, 1, 1)
 	plot.write('wisemap.png')
+
+	plot.color = 'black'
+	plot.plot('fill')
+	plot.color = 'white'
+	plot.op = CAIRO_OPERATOR_ADD
+	plot.apply_settings()
+	img = plot.image
+	img.image_low = 0.
+	img.image_high = 1e3
+	img.resample = 1
+
+	ps = PlotSequence('wise', format='%03i')
+
+	for sid,fnum in zip(T.scan_id[I], T.frame_num[I]):
+		print 'scan,frame', sid, fnum
+		band = 1
+		fn = get_l1b_file(sid, fnum, band)
+		print '-->', fn
+		assert(os.path.exists(fn))
+
+		#I = pyfits.open(fn)[0].data
+		#print 'img min,max,median', I.min(), I.max(), np.median(I.ravel())
+		
+		img.set_wcs_file(fn, 0)
+		img.set_file(fn)
+		plot.plot('image')
+		pfn = ps.getnext()
+		plot.write(pfn)
+		print 'Wrote', pfn
