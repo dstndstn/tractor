8a29ae4e06a54f3441a3066325163df0cbb9e6f5
Dustin Lang <dstndstn@gmail.com>
2013-10-13 15:51:15 +0000

    debug wise-coverage
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@23737 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- wise-coverage.py
+++ wise-coverage.py
@@ -17,13 +17,17 @@ from astrometry.util.util import *
 from astrometry.blind.plotstuff import *
 
 if __name__ == '__main__':
-    W,H = 4000,2000
+    #    W,H = 4000,2000
+    W,H = 400,200
     plot = Plotstuff(size=(W,H), outformat='png')
     plot.wcs = anwcs_create_allsky_hammer_aitoff(180., 0., W, H)
     out = plot.outline
-    out.stepsize = 508
+    #out.stepsize = 508
+    out.stepsize = 2000
     out.fill = 1
-
+    
+    #log_init(3)
+    
     #im = np.zeros((H,W,4), np.uint8)
 
     wcs = Tan()
@@ -35,11 +39,14 @@ if __name__ == '__main__':
     print 'wcs', wcs
     
     ps = PlotSequence('cov')
-    for nbands in [4,3,2]:
+    #for nbands in [4,3,2]:
+    for nbands in [4]:
         bb = [1,2,3,4][:nbands]
-        for band in bb:
+        #for band in bb:
+        band = 1
+        for fn in ['4band.fits', '4band2.fits']:
             count = np.zeros((H,W), np.int16)
-            fn = 'wise-frames/WISE-l1b-metadata-%iband.fits' % nbands
+            #fn = 'wise-frames/WISE-l1b-metadata-%iband.fits' % nbands
             cols = [('w%i'%band)+c for c in
                     ['crval1','crval2','crpix1','crpix2',
                      'cd1_1','cd1_2','cd2_1','cd2_2', 'naxis1','naxis2']]
@@ -52,16 +59,21 @@ if __name__ == '__main__':
             print 'Read', len(T), 'from', fn
             #wcs = Tan()
 
+            #T = T[1:2]
+
             arrs = [T.get(c).astype(float) for c in cols]
             #r1,d1,r2,d2,r3,d3,r4,d4 = arrs
 
             plot.clear()
             plot.color = 'white'
-            plot.alpha = 1./255.
+            #plot.alpha = 1./255.
+            plot.alpha = 1.
             plot.op = CAIRO_OPERATOR_ADD
 
             N = len(T)
             for i in xrange(N):
+                print
+                print 'WCS', i
                 wcs.set(*[a[i] for a in arrs])
                 #out.wcs = anwcs_new_tan(wcs)
                 plot.plot('outline')
@@ -76,6 +88,10 @@ if __name__ == '__main__':
                 #print '.',
 
                 if i and i % 10000 == 0 or i == N-1:
+
+                    fn = ps.getnext()
+                    plot.write(fn)
+                    
                     print 'exposure', i, 'of', N
                     im = plot.get_image_as_numpy()
                     #plot.get_image_as_numpy(out=im)
@@ -86,7 +102,14 @@ if __name__ == '__main__':
                     del im
                     print 'total max:', count.max()
                     plot.clear()
-
+                    
             ofn = 'cov-n%i-b%i.fits' % (nbands, band)
-            fitsio.write(ofn, count)
+            fitsio.write(ofn, count, clobber=True)
             print 'Wrote', ofn
+
+            plt.clf()
+            plt.imshow(count, interpolation='nearest', origin='lower',
+                       vmin=0, vmax=1, cmap='gray')
+            ps.savefig()
+            
+            #sys.exit(0)
