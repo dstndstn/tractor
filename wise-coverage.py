4da325ee9303c2119019c678e78e2ea953d6b575
Dustin Lang <dstndstn@gmail.com>
2013-10-13 02:21:38 +0000

    wise cov
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/projects/tractor@23733 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- wise-coverage.py
+++ wise-coverage.py
@@ -6,6 +6,7 @@ import numpy as np
 import pylab as plt
 import os
 import sys
+import fitsio
 
 from astrometry.util.file import *
 from astrometry.util.fits import *
@@ -20,29 +21,31 @@ if __name__ == '__main__':
     plot = Plotstuff(size=(W,H), outformat='png')
     plot.wcs = anwcs_create_allsky_hammer_aitoff(0., 0., W, H)
     out = plot.outline
-    out.stepsize = 2000
+    out.stepsize = 508
     out.fill = 1
+
+    im = np.zeros((H,W,4), np.uint8)
 
     ps = PlotSequence('cov')
     for nbands in [4,3,2]:
         bb = [1,2,3,4][:nbands]
         for band in bb:
-            count = np.zeros((H,W), int)
+            count = np.zeros((H,W), np.int16)
             fn = 'wise-frames/WISE-l1b-metadata-%iband.fits' % nbands
-            # cols = [('w%i'%band)+c for c in
-            #         ['crval1','crval2','crpix1','crpix2',
-            #          'cd1_1','cd1_2','cd2_1','cd2_2', 'naxis1','naxis2']]
-
             cols = [('w%i'%band)+c for c in
-                    ['ra1','dec1','ra2','dec2','ra3','dec3','ra4','dec4']]
+                    ['crval1','crval2','crpix1','crpix2',
+                     'cd1_1','cd1_2','cd2_1','cd2_2', 'naxis1','naxis2']]
 
+            #cols = [('w%i'%band)+c for c in
+            #        ['ra1','dec1','ra2','dec2','ra3','dec3','ra4','dec4']]
+
+            print 'Reading', fn
             T = fits_table(fn, columns=cols)
             print 'Read', len(T), 'from', fn
             wcs = Tan()
-            #out.wcs = anwcs_new_tan(wcs)
 
             arrs = [T.get(c).astype(float) for c in cols]
-            r1,d1,r2,d2,r3,d3,r4,d4 = arrs
+            #r1,d1,r2,d2,r3,d3,r4,d4 = arrs
 
             plot.clear()
             plot.color = 'white'
@@ -51,34 +54,31 @@ if __name__ == '__main__':
 
             N = len(T)
             for i in xrange(N):
-                #wcs.set(*[a[i] for a in arrs])
-                #plot.plot('outline')
+                wcs.set(*[a[i] for a in arrs])
+                out.wcs = anwcs_new_tan(wcs)
+                plot.plot('outline')
 
-                # plot.move_to_radec(T.ra1[i], T.dec1[i])
-                # plot.line_to_radec(T.ra2[i], T.dec2[i])
-                # plot.line_to_radec(T.ra3[i], T.dec3[i])
-                # plot.line_to_radec(T.ra4[i], T.dec4[i])
-                plot.move_to_radec(r1[i], d1[i])
-                plot.line_to_radec(r2[i], d2[i])
-                plot.line_to_radec(r3[i], d3[i])
-                plot.line_to_radec(r4[i], d4[i])
-                plot.close_path()
-                plot.fill()
+                # plot.move_to_radec(r1[i], d1[i])
+                # plot.line_to_radec(r2[i], d2[i])
+                # plot.line_to_radec(r3[i], d3[i])
+                # plot.line_to_radec(r4[i], d4[i])
+                # plot.close_path()
+                # plot.fill()
 
-                if i and i % 1000 == 0 or i == N-1:
+                #print '.',
+
+                if i and i % 10000 == 0 or i == N-1:
                     print 'exposure', i, 'of', N
-                    im = plot.get_image_as_numpy()
+                    #im = plot.get_image_as_numpy()
+                    plot.get_image_as_numpy(out=im)
                     #print 'im:', im.shape
                     print 'max:', im.max()
                     im = im[:,:,0]
                     count += im
+                    del im
                     print 'total max:', count.max()
                     plot.clear()
 
-                    # plot.op = CAIRO_OPERATOR_OVER
-                    # plot.color = 'black'
-                    # plot.plot('fill')
-                    # plot.color = 'white'
-                    # plot.op = CAIRO_OPERATOR_ADD
-
-            fitsio.write('cov-n%i-b%i.fits' % (nbands, band), count)
+            ofn = 'cov-n%i-b%i.fits' % (nbands, band)
+            fitsio.write(ofn, count)
+            print 'Wrote', ofn
