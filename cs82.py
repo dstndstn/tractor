40ad7820987c0c5b3880b91b821abd9de170dd86
dstn <dstn@03a3cea6-2c03-0410-ac9b-9271b1c66c29>
2013-12-18 18:55:10 +0000

    reindent; make plots
    git-svn-id: http://astrometry.net/svn/trunk/projects/tractor@24270 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- cs82.py
+++ cs82.py
@@ -22,70 +22,73 @@ from tractor.emfit import em_fit_2d
 from tractor.fitpsf import em_init_params
 
 data_dir = 'data/cs82'
+window_flist = 'window_flist.fits'
 
 def get_cs82_sources(T, maglim=25, bands=['u','g','r','i','z']):
-	srcs = Catalog()
-	isrcs = []
-	for i,t in enumerate(T):
-		if t.chi2_psf < t.chi2_model and t.mag_psf <= maglim:
-			#print 'PSF'
-			themag = t.mag_psf
-			nm = NanoMaggies.magToNanomaggies(themag)
-			m = NanoMaggies(order=bands, **dict([(k, nm) for k in bands]))
-			srcs.append(PointSource(RaDecPos(t.ra, t.dec), m))
-			isrcs.append(i)
-			continue
+    srcs = Catalog()
+    isrcs = []
+    for i,t in enumerate(T):
+        if t.chi2_psf < t.chi2_model and t.mag_psf <= maglim:
+            #print 'PSF'
+            themag = t.mag_psf
+            nm = NanoMaggies.magToNanomaggies(themag)
+            m = NanoMaggies(order=bands, **dict([(k, nm) for k in bands]))
+            srcs.append(PointSource(RaDecPos(t.ra, t.dec), m))
+            isrcs.append(i)
+            continue
 
-		if t.mag_disk > maglim and t.mag_spheroid > maglim:
-			#print 'Faint'
-			continue
+        if t.mag_disk > maglim and t.mag_spheroid > maglim:
+            #print 'Faint'
+            continue
 
-		# deV: spheroid
-		# exp: disk
+        # deV: spheroid
+        # exp: disk
 
-		dmag = t.mag_spheroid
-		emag = t.mag_disk
+        dmag = t.mag_spheroid
+        emag = t.mag_disk
 
-		# SPHEROID_REFF [for Sersic index n= 1] = 1.68 * DISK_SCALE
+        # SPHEROID_REFF [for Sersic index n= 1] = 1.68 * DISK_SCALE
 
-		if dmag <= maglim:
-			shape_dev = GalaxyShape(t.spheroid_reff_world * 3600.,
-									t.spheroid_aspect_world,
-									t.spheroid_theta_world + 90.)
+        if dmag <= maglim:
+            shape_dev = GalaxyShape(t.spheroid_reff_world * 3600.,
+                                    t.spheroid_aspect_world,
+                                    t.spheroid_theta_world + 90.)
 
-		if emag <= maglim:
-			shape_exp = GalaxyShape(t.disk_scale_world * 1.68 * 3600.,
-									t.disk_aspect_world,
-									t.disk_theta_world + 90.)
+        if emag <= maglim:
+            shape_exp = GalaxyShape(t.disk_scale_world * 1.68 * 3600.,
+                                    t.disk_aspect_world,
+                                    t.disk_theta_world + 90.)
 
-		pos = RaDecPos(t.alphamodel_j2000, t.deltamodel_j2000)
+        pos = RaDecPos(t.alphamodel_j2000, t.deltamodel_j2000)
 
-		isrcs.append(i)
-		if emag > maglim and dmag <= maglim:
-			nm = NanoMaggies.magToNanomaggies(dmag)
-			m_dev = NanoMaggies(order=bands, **dict([(k, nm) for k in bands]))
-			srcs.append(DevGalaxy(pos, m_dev, shape_dev))
-			continue
-		if emag <= maglim and dmag > maglim:
-			nm = NanoMaggies.magToNanomaggies(emag)
-			m_exp = NanoMaggies(order=bands, **dict([(k, nm) for k in bands]))
-			srcs.append(ExpGalaxy(pos, m_exp, shape_exp))
-			continue
+        isrcs.append(i)
+        if emag > maglim and dmag <= maglim:
+            nm = NanoMaggies.magToNanomaggies(dmag)
+            m_dev = NanoMaggies(order=bands, **dict([(k, nm) for k in bands]))
+            srcs.append(DevGalaxy(pos, m_dev, shape_dev))
+            continue
+        if emag <= maglim and dmag > maglim:
+            nm = NanoMaggies.magToNanomaggies(emag)
+            m_exp = NanoMaggies(order=bands, **dict([(k, nm) for k in bands]))
+            srcs.append(ExpGalaxy(pos, m_exp, shape_exp))
+            continue
 
         # print 'Composite'
         nmd = NanoMaggies.magToNanomaggies(dmag)
-		nme = NanoMaggies.magToNanomaggies(emag)
-		nm = nmd + nme
-		fdev = (nmd / nm)
-		m = NanoMaggies(order=bands, **dict([(k, nm) for k in bands]))
-		srcs.append(FixedCompositeGalaxy(pos, m, fdev, shape_exp, shape_dev))
+        nme = NanoMaggies.magToNanomaggies(emag)
+        nm = nmd + nme
+        fdev = (nmd / nm)
+        m = NanoMaggies(order=bands, **dict([(k, nm) for k in bands]))
+        srcs.append(FixedCompositeGalaxy(pos, m, fdev, shape_exp, shape_dev))
 
-	#print 'Sources:', len(srcs)
-	return srcs, np.array(isrcs)
+    #print 'Sources:', len(srcs)
+    return srcs, np.array(isrcs)
 
 
 def getTables(cs82field, enclosed=True, extra_cols=[]):
-	T = fits_table(os.path.join(data_dir, '%s_y.V2.7A.swarp.cut.deVexp.fit' % cs82field),
+    fn = os.path.join(data_dir, 'masked.%s_y.V2.7A.swarp.cut.deVexp.fit' % cs82field)
+    print 'Reading', fn
+    T = fits_table(fn,
             hdu=2, column_map={'ALPHA_J2000':'ra',
                                'DELTA_J2000':'dec'},
             columns=[x.upper() for x in
@@ -95,133 +98,151 @@ def getTables(cs82field, enclosed=True, extra_cols=[]):
                       'disk_theta_world', 'spheroid_reff_world',
                       'spheroid_aspect_world', 'spheroid_theta_world',
                       'alphamodel_j2000', 'deltamodel_j2000'] + extra_cols])
-	ra0,ra1 = T.ra.min(), T.ra.max()
-	dec0,dec1 = T.dec.min(), T.dec.max()
-	print 'RA', ra0,ra1
-	print 'Dec', dec0,dec1
-	T.index = np.arange(len(T))
+    ra0,ra1 = T.ra.min(), T.ra.max()
+    dec0,dec1 = T.dec.min(), T.dec.max()
+    print 'RA', ra0,ra1
+    print 'Dec', dec0,dec1
+    T.index = np.arange(len(T))
 
-	# ASSUME no RA wrap-around in the catalog
-	trad = 0.5 * np.hypot(ra1 - ra0, dec1 - dec0)
-	tcen = radectoxyz((ra1+ra0)*0.5, (dec1+dec0)*0.5)
+    # ASSUME no RA wrap-around in the catalog
+    trad = 0.5 * np.hypot(ra1 - ra0, dec1 - dec0)
+    tcen = radectoxyz((ra1+ra0)*0.5, (dec1+dec0)*0.5)
 
-	frad = 0.5 * np.hypot(13., 9.) / 60.
+    frad = 0.5 * np.hypot(13., 9.) / 60.
 
-	fn = 'sdssfield-%s.fits' % cs82field
-	if os.path.exists(fn):
-		print 'Reading', fn
-		F = fits_table(fn)
-	else:
-		F = fits_table('window_flist-DR9.fits')
+    fn = 'sdssfield-%s.fits' % cs82field
+    if os.path.exists(fn):
+        print 'Reading', fn
+        F = fits_table(fn)
+    else:
+        F = fits_table(window_flist)
 
-		# These runs don't appear in DAS
-		#F.cut((F.run != 4322) * (F.run != 4240) * (F.run != 4266))
-		F.cut(F.rerun != "157")
+        # These runs don't appear in DAS
+        #F.cut((F.run != 4322) * (F.run != 4240) * (F.run != 4266))
+        F.cut(F.rerun != "157")
 
-		# For Stripe 82, mu-nu is aligned with RA,Dec.
-		rd = []
-		rd.append(munu_to_radec_deg(F.mu_start, F.nu_start, F.node, F.incl))
-		rd.append(munu_to_radec_deg(F.mu_end,   F.nu_end,   F.node, F.incl))
-		rd = np.array(rd)
-		F.ra0  = np.min(rd[:,0,:], axis=0)
-		F.ra1  = np.max(rd[:,0,:], axis=0)
-		F.dec0 = np.min(rd[:,1,:], axis=0)
-		F.dec1 = np.max(rd[:,1,:], axis=0)
+        # For Stripe 82, mu-nu is aligned with RA,Dec.
+        rd = []
+        rd.append(munu_to_radec_deg(F.mu_start, F.nu_start, F.node, F.incl))
+        rd.append(munu_to_radec_deg(F.mu_end,   F.nu_end,   F.node, F.incl))
+        rd = np.array(rd)
+        F.ra0  = np.min(rd[:,0,:], axis=0)
+        F.ra1  = np.max(rd[:,0,:], axis=0)
+        F.dec0 = np.min(rd[:,1,:], axis=0)
+        F.dec1 = np.max(rd[:,1,:], axis=0)
 
-		I = np.flatnonzero((F.ra0 <= T.ra.max()) *
-						   (F.ra1 >= T.ra.min()) *
-						   (F.dec0 <= T.dec.max()) *
-						   (F.dec1 >= T.dec.min()))
-		print 'Possibly overlapping fields:', len(I)
-		F.cut(I)
+        I = np.flatnonzero((F.ra0 <= T.ra.max()) *
+                           (F.ra1 >= T.ra.min()) *
+                           (F.dec0 <= T.dec.max()) *
+                           (F.dec1 >= T.dec.min()))
+        print 'Possibly overlapping fields:', len(I)
+        F.cut(I)
 
-		# When will I ever learn not to cut on RA boxes when there is wrap-around?
-		xyz = radectoxyz(F.ra, F.dec)
-		r2 = np.sum((xyz - tcen)**2, axis=1)
-		I = np.flatnonzero(r2 < deg2distsq(trad + frad))
-		print 'Possibly overlapping fields:', len(I)
-		F.cut(I)
+        # When will I ever learn not to cut on RA boxes when there is wrap-around?
+        xyz = radectoxyz(F.ra, F.dec)
+        r2 = np.sum((xyz - tcen)**2, axis=1)
+        I = np.flatnonzero(r2 < deg2distsq(trad + frad))
+        print 'Possibly overlapping fields:', len(I)
+        F.cut(I)
 
-		F.enclosed = ((F.ra0 >= T.ra.min()) *
-					  (F.ra1 <= T.ra.max()) *
-					  (F.dec0 >= T.dec.min()) *
-					  (F.dec1 <= T.dec.max()))
-		
-		# Sort by distance from the center of the field.
-		ra  = (T.ra.min()  + T.ra.max() ) / 2.
-		dec = (T.dec.min() + T.dec.max()) / 2.
-		I = np.argsort( ((F.ra0  + F.ra1 )/2. - ra )**2 +
-						((F.dec0 + F.dec1)/2. - dec)**2 )
-		F.cut(I)
+        F.enclosed = ((F.ra0 >= T.ra.min()) *
+                      (F.ra1 <= T.ra.max()) *
+                      (F.dec0 >= T.dec.min()) *
+                      (F.dec1 <= T.dec.max()))
+        
+        # Sort by distance from the center of the field.
+        ra  = (T.ra.min()  + T.ra.max() ) / 2.
+        dec = (T.dec.min() + T.dec.max()) / 2.
+        I = np.argsort( ((F.ra0  + F.ra1 )/2. - ra )**2 +
+                        ((F.dec0 + F.dec1)/2. - dec)**2 )
+        F.cut(I)
 
-		F.writeto(fn)
-		print 'Wrote', fn
+        F.writeto(fn)
+        print 'Wrote', fn
 
-	if enclosed:
-		F.cut(F.enclosed)
-		print 'Enclosed fields:', len(F)
-		
-	return T,F
+    if enclosed:
+        F.cut(F.enclosed)
+        print 'Enclosed fields:', len(F)
+        
+    return T,F
 
 
 def main(opt, cs82field):
     bands = opt.bands
+
+    ps = PlotSequence('cs82')
 
     version = get_svn_version()
     print 'SVN version info:', version
     
     T,F = getTables(cs82field, enclosed=False)
 
-	# We probably have to work in Dec slices to keep the memory reasonable
-	dec0 = T.dec.min()
-	dec1 = T.dec.max()
-	print 'Dec range:', dec0, dec1
-	#nslices = 4
-	#ddec = (dec1 - dec0) / nslices
-	#print 'ddec:', ddec
+    # We probably have to work in Dec slices to keep the memory reasonable
+    dec0 = T.dec.min()
+    dec1 = T.dec.max()
+    print 'Dec range:', dec0, dec1
+    #nslices = 4
+    #ddec = (dec1 - dec0) / nslices
+    #print 'ddec:', ddec
 
     #sdss = DR9(basedir='cs82data/dr9')
     sdss = DR9()
     sdss.useLocalTree()
     sdss.saveUnzippedFiles('data/unzip')
 
-	### HACK -- ignore 0/360 issues
-	ra0 = T.ra.min()
-	ra1 = T.ra.max()
-	print 'RA range:', ra0, ra1
-	assert(ra1 - ra0 < 2.)
+    ### HACK -- ignore 0/360 issues
+    ra0 = T.ra.min()
+    ra1 = T.ra.max()
+    print 'RA range:', ra0, ra1
+    assert(ra1 - ra0 < 2.)
 
-	decs = np.linspace(dec0, dec1, 5)
-	ras  = np.linspace(ra0,  ra1, 5)
+    plt.clf()
+    plothist(T.ra, T.dec, 200, imshowargs=dict(cmap='gray'))
+    #plt.plot([ra0,ra0,ra1,ra1,ra0], [dec0,dec1,dec1,dec0,dec0], 'r-')
+    for f in F:
+        plt.plot([f.ra0,f.ra0,f.ra1,f.ra1,f.ra0], [f.dec0,f.dec1,f.dec1,f.dec0,f.dec0], 'b-', alpha=0.5)
+    plt.title('%s: %i SDSS fields' % (cs82field, len(F)))
+    ps.savefig()
 
-	print 'Score range:', F.score.min(), F.score.max()
-	print 'Before score cut:', len(F)
-	F.cut(F.score > 0.5)
-	print 'Cut on score:', len(F)
+
+    decs = np.linspace(dec0, dec1, 5)
+    ras  = np.linspace(ra0,  ra1, 5)
+
+    print 'Score range:', F.score.min(), F.score.max()
+    print 'Before score cut:', len(F)
+    F.cut(F.score > 0.5)
+    print 'Cut on score:', len(F)
     
-	for decslice,(dlo,dhi) in enumerate(zip(decs, decs[1:])):
-		print 'Dec slice:', dlo, dhi
-		for raslice,(rlo,rhi) in enumerate(zip(ras, ras[1:])):
-			print 'RA slice:', rlo, rhi
+    for decslice,(dlo,dhi) in enumerate(zip(decs, decs[1:])):
+        print 'Dec slice:', dlo, dhi
+        for raslice,(rlo,rhi) in enumerate(zip(ras, ras[1:])):
+            print 'RA slice:', rlo, rhi
 
-			# in deg
-			margin = 15. / 3600.
-			Ti = T[((T.dec + margin) >= dlo) * ((T.dec - margin) <= dhi) *
-				   ((T.ra  + margin) >= rlo) * ((T.ra  - margin) <= rhi)]
-			Ti.marginal = np.logical_not((Ti.dec >= dlo) * (Ti.dec <= dhi) *
-										 (Ti.ra  >= rlo) * (Ti.ra  <= rhi))
-			print len(Ti), 'sources in RA,Dec slice'
-			print len(np.flatnonzero(Ti.marginal)), 'are in the margins'
+            # in deg
+            margin = 15. / 3600.
+            Ti = T[((T.dec + margin) >= dlo) * ((T.dec - margin) <= dhi) *
+                   ((T.ra  + margin) >= rlo) * ((T.ra  - margin) <= rhi)]
+            Ti.marginal = np.logical_not((Ti.dec >= dlo) * (Ti.dec <= dhi) *
+                                         (Ti.ra  >= rlo) * (Ti.ra  <= rhi))
+            print len(Ti), 'sources in RA,Dec slice'
+            print len(np.flatnonzero(Ti.marginal)), 'are in the margins'
 
-			Fi = F[np.logical_not(np.logical_or(F.dec0 > dhi, F.dec1 < dlo)) *
-				   np.logical_not(np.logical_or(F.ra0  > rhi, F.ra1  < rlo))]
-			print len(Fi), 'fields in RA,Dec slice'
+            Fi = F[np.logical_not(np.logical_or(F.dec0 > dhi, F.dec1 < dlo)) *
+                   np.logical_not(np.logical_or(F.ra0  > rhi, F.ra1  < rlo))]
+            print len(Fi), 'fields in RA,Dec slice'
 
+            plt.clf()
+            plothist(Ti.ra, Ti.dec, 200, imshowargs=dict(cmap='gray'))
+            plt.plot([rlo,rlo,rhi,rhi,rlo], [dlo,dhi,dhi,dlo,dlo], 'r-')
+            for f in Fi:
+                plt.plot([f.ra0,f.ra0,f.ra1,f.ra1,f.ra0], [f.dec0,f.dec1,f.dec1,f.dec0,f.dec0], 'b-', alpha=0.5)
+            plt.title('%s slice d%i r%i: %i SDSS fields' % (cs82field, decslice, raslice, len(Fi)))
+            ps.savefig()
 
-			print 'Creating Tractor sources...'
-			maglim = 24
-			cat,icat = get_cs82_sources(Ti, maglim=maglim, bands=bands)
-			print 'Got', len(cat), 'sources'
+            print 'Creating Tractor sources...'
+            maglim = 24
+            cat,icat = get_cs82_sources(Ti, maglim=maglim, bands=bands)
+            print 'Got', len(cat), 'sources'
 
             # FIXME -- initialize fluxes by matching to SDSS sources?
             # FIXME -- freeze marginal sources!
@@ -232,6 +253,7 @@ def main(opt, cs82field):
 
                 tims = []
                 sigs = []
+                npix = 0
                 for i,(r,c,f) in enumerate(zip(Fi.run, Fi.camcol, Fi.field)):
                     print 'Reading', (i+1), 'of', len(Fi), ':', r,c,f,band
                     tim,inf = get_tractor_image_dr9(r, c, f, band, sdss=sdss,
@@ -241,6 +263,7 @@ def main(opt, cs82field):
                     if tim is None:
                         continue
                     (H,W) = tim.shape
+                    print 'Tim', tim.shape
                     tim.wcs.setConstantCd(W/2., H/2.)
                     del tim.origInvvar
                     del tim.starMask
@@ -256,6 +279,17 @@ def main(opt, cs82field):
 
                 print 'Read', len(tims), 'images'
                 print 'total of', npix, 'pixels'
+
+                plt.clf()
+                plothist(Ti.ra, Ti.dec, 200, imshowargs=dict(cmap='gray'))
+                plt.plot([rlo,rlo,rhi,rhi,rlo], [dlo,dhi,dhi,dlo,dlo], 'r-')
+                for tim in tims:
+                    H,W = tim.shape
+                    rd0 = tim.getWcs().pixelToPosition(0,0)
+                    rd1 = tim.getWcs().pixelToPosition(W-1,H-1)
+                    plt.plot([rd0.ra,rd0.ra,rd1.ra,rd1.ra,rd0.ra], [rd0.dec,rd1.dec,rd1.dec,rd0.dec,rd0.dec], 'b-', alpha=0.5)
+                plt.title('%s slice d%i r%i: %i SDSS fields' % (cs82field, decslice, raslice, len(tims)))
+                ps.savefig()
 
                 #minsig = getattr(opt, 'minsig%i' % band)
                 #minsb = sig1 * minsig
@@ -279,14 +313,14 @@ def main(opt, cs82field):
                 
 
 if __name__ == '__main__':
-	import optparse
-	parser = optparse.OptionParser('%prog [options]')
+    import optparse
+    parser = optparse.OptionParser('%prog [options]')
     parser.add_option('-b', dest='bands', type=str, default='ugriz',
                       help='SDSS bands (default %default)')
-	opt,args = parser.parse_args()
+    opt,args = parser.parse_args()
 
     lvl = logging.INFO
     logging.basicConfig(level=lvl, format='%(message)s', stream=sys.stdout)
 
-	cs82field = 'S82p18p'
-	main(opt, cs82field)
+    cs82field = 'S82p18p'
+    main(opt, cs82field)
