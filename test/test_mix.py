fix mixture-profile derivatives

test/test_mix.py
@@ -21,32 +21,47 @@ print 'PSF:', gpsf
 ps = PlotSequence('test-mix')
 
 patch0 = gpsf.getPointSourcePatch(0., 0., radius=20)
-patch1 = gpsf.getPointSourcePatch(0., 0., radius=20, v3=True, minval=1e-6)
+
+approx = 1e-4
+
+patch1 = gpsf.getPointSourcePatch(0., 0., radius=20, v3=True, minval=approx)
 patch2,dx2,dy2 = gpsf.getPointSourcePatch(0., 0., radius=20, v3=True,
-                                          minval=1e-6, derivs=True)
+                                          minval=approx, derivs=True)
 
 mn,mx = patch0.patch.min(), patch0.patch.max()
-ima = dict(vmin=mn, vmax=mx)
-imda = dict(vmin=-1e-6, vmax=1e-6)
+#ima = dict(vmin=mn, vmax=mx)
+floor = approx * 1e-2
+ima = dict(vmin=np.log10(max(floor, mn)), vmax=np.log10(mx))
+
+imda = dict(vmin=-approx, vmax=approx)
 
 for patch in [patch1, patch2]:
     print 'Patch range', patch.patch.min(), patch.patch.max()
     plt.clf()
     plt.subplot(2,2,1)
-    dimshow(patch0.patch, **ima)
+    dimshow(np.log10(np.maximum(floor, patch0.patch)), **ima)
+    plt.colorbar()
     plt.title('p0')
     plt.subplot(2,2,2)
-    dimshow(patch.patch, **ima)
+    dimshow(np.log10(np.maximum(floor, patch.patch)), **ima)
+    plt.colorbar()
+
+    plt.subplot(2,2,4)
+    dimshow(patch.patch > 0, vmin=0, vmax=1)
+    
     plt.title('patch')
     plt.subplot(2,2,3)
-    dimshow(patch.patch - patch0.patch, **imda)
+    diff = patch.patch - patch0.patch
+    print 'Diff range:', diff.min(), diff.max()
+    dimshow(diff, **imda)
     plt.colorbar()
     plt.title('difference')
     ps.savefig()
 
+
 plt.clf()
 plt.subplot(2,2,2)
-dimshow(patch2.patch, **ima)
+dimshow(np.log10(np.maximum(floor, patch2.patch)), **ima)
 plt.subplot(2,2,3)
 dimshow(dx2.patch)
 plt.subplot(2,2,4)

tractor/basics.py
@@ -1340,11 +1340,12 @@ class GaussianMixturePSF(ParamList, ducks.ImageCalibration):
                 if derivs:
                     xderiv = np.zeros_like(result)
                     yderiv = np.zeros_like(result)
-                    rtn = c_gauss_2d_approx3(x0, x1+1, y0, y1+1, px, py, minval,
-                                             self.mog.amp, self.mog.mean,
-                                             self.mog.var, result,
-                                             xderiv, yderiv, mask,
-                                             int(px), int(py), minradius)
+                rtn = c_gauss_2d_approx3(x0, x1+1, y0, y1+1, px, py, minval,
+                                         self.mog.amp, self.mog.mean,
+                                         self.mog.var, result,
+                                         xderiv, yderiv, mask,
+                                         int(px), int(py), minradius)
+                assert(rtn == 0)
                 if derivs:
                     return (Patch(x0,y0,result), Patch(x0,y0,xderiv),
                             Patch(x0,y0,yderiv))