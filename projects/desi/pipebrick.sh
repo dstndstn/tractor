f7de33eb08582f8e640ce73da755ef1944016d97
Dustin Lang <dstndstn@gmail.com>
2014-10-30 11:49:46 -0700

    hmm, pipebrick.sh wasn't already added?

---
+++ projects/desi/pipebrick.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "PWD: $(pwd)"
+export PYTHONPATH=${PYTHONPATH}:.
+echo "Modules:"
+module list 2>&1
+echo
+
+brick="$1"
+python -u projects/desi/pipebrick.py $brick > pipebrick-logs/$brick.log 2>&1
+
+# qdo launch bricks 1 --batchopts "-l pvmem=6GB -A cosmo -t 1-20 -q serial" --walltime=24:00:00 --script projects/desi/pipebrick.sh --verbose
+
