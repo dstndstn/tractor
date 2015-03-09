d8ec5bcd5ad9fbcd8f261963f679b4dc496e5024
Dustin Lang <dstndstn@gmail.com>
2015-03-09 12:01:28 -0700

    notes on edison

--- projects/desi/pipebrick.sh
+++ projects/desi/pipebrick.sh
@@ -2,21 +2,20 @@
 
 export PYTHONPATH=${PYTHONPATH}:.
 
-# echo "PWD: $(pwd)"
-# echo "Modules:"
-# module list 2>&1
-# echo
-# 
-# echo "Environment:"
-# set
+echo "PWD: $(pwd)"
+echo "Modules:"
+module list 2>&1
+echo
+echo "Environment:"
+set
+echo
+
 # 
 # echo MKL $MKL
 # echo MKL_HOME $MKL_HOME
 # echo LD_LIBRARY_PATH $LD_LIBRARY_PATH
-# 
 # echo ldd:
 # ldd tractor/_ceres.so
-# 
 # python -c "from tractor.ceres import *"
 
 # Force MKL single-threaded
@@ -29,7 +28,11 @@ outdir=edr4
 mkdir -p $outdir/logs
 
 #python -u projects/desi/runbrick.py --force-all --no-write --stage writecat --brick $brick --outdir $outdir > $outdir/logs/$brick.log 2>&1
+
 python -u projects/desi/runbrick.py --force-all --no-write --stage writecat --brick $brick --outdir $outdir --threads 8 > $outdir/logs/$brick.log 2>&1
+
+# Try 8 threads on edison nodes (packing 3 of those per 24-core node)?
+#qdo launch bricks 3 --mpack 8 --batchopts "-A desi -t 1-10" --walltime=24:00:00 --script projects/desi/pipebrick-edison.sh --batchqueue regular
 
 # with 8 threads: 3 GB per core * 8 cores (most of the carver nodes have 24 GB)
 # qdo launch bricks 1 --batchopts "-l pvmem=3GB -l nodes=1:ppn=8 -A desi -t 1-20 -q regular" --walltime=48:00:00 --script projects/desi/pipebrick.sh
