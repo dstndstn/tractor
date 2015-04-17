5efcb02ff70a3782e10afdcfa03a2df91c97753a
Dustin Lang <dstndstn@gmail.com>
2015-04-16 20:05:52 -0700

    prep for dr1j

--- projects/desi/pipebrick.sh
+++ projects/desi/pipebrick.sh
@@ -6,7 +6,7 @@ export PYTHONPATH=${PYTHONPATH}:.
 # https://software.intel.com/en-us/articles/using-threaded-intel-mkl-in-multi-thread-application
 export MKL_NUM_THREADS=1
 
-outdir=$SCRATCH/dr1h
+outdir=$SCRATCH/dr1j
 
 brick="$1"
 
@@ -32,7 +32,7 @@ echo >> $log
 echo -e "\nStarting on ${NERSC_HOST} $(hostname)\n" >> $log
 echo "-----------------------------------------------------------------------------------------" >> $log
 
-python projects/desi/runbrick.py --force-all --no-write --brick $brick --outdir $outdir --threads 4 --nsigma 6 --skip --pipe >> $log 2>&1
+python projects/desi/runbrick.py --force-all --no-write --brick $brick --outdir $outdir --threads 3 --nsigma 6 --skip --pipe >> $log 2>&1
 
 
 # Edison: 6 threads per job, 4 jobs per node = 24 cores.
