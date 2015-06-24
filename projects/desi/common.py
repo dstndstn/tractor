913a05922a1eab09e18819494943bf0c6355be1a
Dustin Lang <dstndstn@gmail.com>
2015-06-24 17:04:11 -0400

    add some doco for get_Rgb

--- projects/desi/common.py
+++ projects/desi/common.py
@@ -797,6 +797,14 @@ def get_rgb(imgs, bands, mnmx=None, arcsinh=None, scales=None):
     '''
     Given a list of images in the given bands, returns a scaled RGB
     image.
+
+    *imgs*  a list of numpy arrays, all the same size, in nanomaggies
+    *bands* a list of strings, eg, ['g','r','z']
+    *mnmx*  = (min,max), values that will become black/white *after* scaling.
+           Default is (-3,10)
+    *arcsinh* use nonlinear scaling as in SDSS
+    *scales*
+
     '''
     bands = ''.join(bands)
 
