fb70a92389d3ee0b545a97b82ad658fb903b5447
Dustin Lang <dstndstn@gmail.com>
2015-09-15 17:52:50 -0400

    add fastexp code from ngmix

--- tractor/fastexp-test.py
+++ tractor/fastexp-test.py
@@ -14,12 +14,14 @@ if __name__ == '__main__':
 
     e = np.exp(xx)
     fe = np.array([fastexp.my_fastexp(x) for x in xx])
+    ng = np.array([fastexp.ngmix_exp(x) for x in xx])
 
     ps = PlotSequence('fastexp')
     
     plt.clf()
-    plt.plot(xx, e, 'b-', alpha=0.25, lw=3)
+    plt.plot(xx, e, 'k-', alpha=0.25, lw=3)
     plt.plot(xx, fe, 'r-')
+    plt.plot(xx, ng, 'b-')
     ps.savefig()
 
     plt.yscale('log')
@@ -28,8 +30,10 @@ if __name__ == '__main__':
     plt.clf()
     plt.axhline(0., color='b', alpha=0.25, lw=3)
     v = fe / e - 1.
-    mx = max(np.abs(v))
     plt.plot(xx, v, 'r-')
+    v2 = ng / e - 1.
+    plt.plot(xx, v2, 'b-')
+    mx = max(np.abs(v))
     plt.ylim(-1.1*mx, 1.1*mx)
     ps.savefig()
     
--- tractor/fastexp.i
+++ tractor/fastexp.i
@@ -8,4 +8,27 @@ float my_fastexp(double x) {
     return fastexp(x);
 }
 
+
+
+/// from ngmix
+#include "fastexp-table.h"
+
+union fmath_di {
+    double d;
+    uint64_t i;
+};
+
+double ngmix_exp(double x) {
+    union fmath_di di;
+    di.d = x * a + b;
+    uint64_t iax = dtbl[di.i & sbit_masked];
+    double t = (di.d - b) * ra - x;
+    uint64_t u = ((di.i + adj) >> sbit) << 52;
+    double y = (C3 - t) * (t * t) * C2 - t + C1;
+    di.i = u | iax;
+    return y * di.d;
+}
+
+
+
  %}
