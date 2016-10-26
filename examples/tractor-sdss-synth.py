b3a29d30b6cc4bee7f639caa482badc967698cdb
Dustin Lang <dstndstn@gmail.com>
2016-10-25 22:09:35 -0400

    sdss synth example: use sdss obj for sources too

--- examples/tractor-sdss-synth.py
+++ examples/tractor-sdss-synth.py
@@ -327,7 +327,7 @@ def main():
         print('Scaling images by', opt.scale)
         tims = [st.scale_sdss_image(tim, opt.scale) for tim in tims]
         
-    sources = getsrc(run, camcol, field, bandname, bands=bands, curl=opt.curl, roi=opt.roi)
+    sources = getsrc(run, camcol, field, bandname, bands=bands, curl=opt.curl, roi=opt.roi, sdss=sdss, retrieve=False, checkFiles=False)
     tractor = Tractor(tims, sources)
 
     sa = dict(debug=opt.debug, plotAll=opt.plotAll, roi=opt.roi)
