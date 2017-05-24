d316026737f934f392007221f1b3128ff8173574
Dustin Lang <dstndstn@gmail.com>
2017-05-24 16:45:33 -0400

    absolute imports for py3

--- tractor/sdss.py
+++ tractor/sdss.py
@@ -830,9 +830,9 @@ def _get_tractor_image_dr8(run, camcol, field, bandname, sdss=None,
         mypsf = PixelizedPSF(klpsf)
         
     elif psf == 'kl-gm':
-        from emfit import em_fit_2d
-        from fitpsf import em_init_params
-        
+        from tractor.emfit import em_fit_2d
+        from tractor.fitpsf import em_init_params
+
         # Create Gaussian mixture model PSF approximation.
         klpsf = psfield.getPsfAtPoints(bandnum, x0+W/2, y0+H/2)
         S = klpsf.shape[0]
