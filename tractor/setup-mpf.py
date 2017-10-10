fd2201621490500483217eb1d8d485c99e696743
Chris Hogan <christopher.hogan@intel.com>
2017-10-10 16:28:45 -0500

    Icc options

--- tractor/setup-mpf.py
+++ tractor/setup-mpf.py
@@ -12,8 +12,8 @@ c_swig_module = Extension('_mp_fourier',
 my_swig_mod = Extension('_c_mp_fourier',
                         sources=['c_mp_fourier.i'],
                         include_dirs=numpy_inc,
-                        extra_compile_args=['-g'],
-                        extra_link_args=['-g'])
+                        extra_compile_args=['-g', '-xhost'],
+                        extra_link_args=['-g', '-limf', '-lsvml'])
 
 setup(name = 'Gaussian mixtures -- Fourier transform',
 	  version = '1.0',
