522bd1cfdf6eec2b10c49db8f969c3d3d2035b05
Dustin Lang <dstn@cloud2b.arbutus>
2023-04-27 18:32:32 +0000

    rename setup-cython.py to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from distutils.core import setup, Extension
-from distutils.command.build_ext import *
-from distutils.dist import Distribution
 import os
+import sys
+from distutils.core import setup, Extension
+from distutils.dist import Distribution
 
 # from http://stackoverflow.com/questions/12491328/python-distutils-not-include-the-swig-generated-module
 from distutils.command.build import build
@@ -13,124 +13,175 @@ class CustomBuild(build):
         ('build_scripts', build.has_scripts),
     ]
 
-import numpy as np
-numpy_inc = [np.get_include()]
+def main():
+    import numpy as np
+    numpy_inc = [np.get_include()]
 
-import sys
-from subprocess import check_output
+    if os.environ.get('CC') == 'icc':
+        compile_args = ['-g']
+        cpp_compile_args = compile_args
+        link_args = ['-g', '-lsvml']
+    else:
+        compile_args = ['-g', '-std=c99']
+        cpp_compile_args = ['-g']
+        link_args = ['-g']
 
-eigen_inc = os.environ.get('EIGEN_INC', None)
-if eigen_inc is None:
+    kwargs = dict(extra_compile_args=compile_args,
+                  extra_link_args=link_args)
+
+    # Swig extensions
+    module_fourier = Extension('tractor._mp_fourier',
+                               sources = ['tractor/mp_fourier.i'],
+                               include_dirs = numpy_inc,
+                               undef_macros=['NDEBUG'],
+                               swig_opts=['-outdir', 'tractor'],
+                               **kwargs)
+    module_mix = Extension('tractor._mix',
+                           sources = ['tractor/mix.i'],
+                           include_dirs = numpy_inc,
+                           extra_objects = [],
+                           undef_macros=['NDEBUG'],
+                           swig_opts=['-outdir', 'tractor'],
+                           **kwargs)
+    module_em = Extension('tractor._emfit',
+                          sources = ['tractor/emfit.i' ],
+                          include_dirs = numpy_inc,
+                          extra_objects = [],
+                          undef_macros=['NDEBUG'],
+                          swig_opts=['-outdir', 'tractor'],
+                          **kwargs)
+    mods = [module_mix, module_em, module_fourier]
+    pymods = ['tractor.mix', 'tractor.emfit', 'tractor.mp_fourier']
+
+    class MyDistribution(Distribution):
+        display_options = Distribution.display_options + [
+            ('with-ceres', None, 'build Ceres module?'),
+            ('with-cython', None, 'build using Cython?'),
+            ]
+
+    key = '--with-ceres'
+    with_ceres = key in sys.argv
+    if with_ceres:
+        sys.argv.remove(key)
+
+        ## Ceres module
+        from subprocess import check_output
+        eigen_inc = os.environ.get('EIGEN_INC', None)
+        if eigen_inc is None:
+            try:
+                eigen_inc = check_output(['pkg-config', '--cflags', 'eigen3']).strip()
+                # py3
+                eigen_inc = eigen_inc.decode()
+            except:
+                eigen_inc = ''
+
+        import shlex
+        inc = shlex.split(eigen_inc)
+        ceres_inc = os.environ.get('CERES_INC', None)
+        ceres_lib = os.environ.get('CERES_LIB', '-lceres')
+        if ceres_inc is not None:
+            inc.extend(shlex.split(ceres_inc))
+        link = []
+        if ceres_lib is not None:
+            link.extend(shlex.split(ceres_lib))
+        module_ceres = Extension('tractor._ceres',
+                                 sources=['tractor/ceres-tractor.cc', 'tractor/ceres.i'],
+                                 include_dirs = numpy_inc,
+                                 extra_compile_args = cpp_compile_args + inc,
+                                 extra_link_args = link_args + link,
+                                 language = 'c++',
+                                 swig_opts=['-c++', '-outdir', 'tractor'],
+                                 )
+        mods.append(module_ceres)
+        pymods.append('tractor.ceres')
+
+    key = '--with-cython'
+    with_cython = key in sys.argv
+    if with_cython:
+        sys.argv.remove(key)
+        from Cython.Build import cythonize
+
+        nthreads = 4
+        comdir2 = dict(language_level=3,
+                       profile=True)
+        # galaxy.py for some reason can't handle infer_types.
+        cymod2 = cythonize(
+            ['tractor/galaxy.py',],
+            annotate=True,
+            compiler_directives=comdir2,
+            nthreads=nthreads)
+
+        comdir1 = dict(language_level=3,
+                       infer_types=True,
+                       profile=True)
+        cymod1 = cythonize(
+            [
+                'tractor/patch.pyx',
+                'tractor/basics.py',
+                'tractor/brightness.py',
+                'tractor/ceres_optimizer.py',
+                'tractor/constrained_optimizer.py',
+                'tractor/dense_optimizer.py',
+                'tractor/ducks.py',
+                'tractor/ellipses.py',
+                'tractor/engine.py',
+                'tractor/image.py',
+                'tractor/imageutils.py',
+                'tractor/lsqr_optimizer.py',
+                'tractor/mixture_profiles.py',
+                'tractor/motion.py',
+                'tractor/optimize.py',
+                'tractor/pointsource.py',
+                'tractor/psf.py',
+                'tractor/psfex.py',
+                'tractor/sersic.py',
+                'tractor/sfd.py',
+                'tractor/shifted.py',
+                'tractor/sky.py',
+                'tractor/splinesky.py',
+                'tractor/tractortime.py',
+                'tractor/utils.py',
+                'tractor/wcs.py',
+            ],
+            annotate=True,
+            compiler_directives=comdir1,
+            nthreads=nthreads)
+
+        # Reach into the distutils.core.Extension objects and set the compiler options...
+        for ext in cymod1 + cymod2:
+            for k,v in kwargs.items():
+                setattr(ext, k, v)
+        mods = mods + cymod1 + cymod2
+
+    cmd = 'echo "git_version = \'$(git describe)\'" > tractor/gitversion.py && cat tractor/gitversion.py tractor/version_post.py > tractor/version.py'
+    print('Running', cmd)
+    rtn = os.system(cmd)
+    print('Return value:', rtn)
+    pymods.append('tractor.version')
+    version = '0.0'
     try:
-        eigen_inc = check_output(['pkg-config', '--cflags', 'eigen3']).strip()
-        # py3
-        eigen_inc = eigen_inc.decode()
+        from tractor.version import version as v
+        version = v
+        print('Set version', version)
     except:
-        eigen_inc = ''
-inc = eigen_inc.split()
-    
-ceres_inc = os.environ.get('CERES_INC', None)
-ceres_lib = os.environ.get('CERES_LIB', None)
-if ceres_inc is not None:
-    inc.append(ceres_inc)
+        import traceback
+        traceback.print_exc()
+        pass
 
-link = []
-if ceres_lib is not None:
-    link.append(ceres_lib)
-
-module_ceres = Extension('tractor._ceres',
-                         sources=['tractor/ceres-tractor.cc', 'tractor/ceres.i'],
-                         include_dirs = numpy_inc,
-                         extra_compile_args = inc + ['-std=c++14'],
-                         extra_link_args = link,
-                         language = 'c++',
-                         swig_opts=['-c++'],
-                         )
-
-module_mix = Extension('tractor._mix',
-                       sources = ['tractor/mix.i'],
-                       include_dirs = numpy_inc,
-                       extra_objects = [],
-                       undef_macros=['NDEBUG'],
+    setup(
+        name='tractor',
+        author='Dustin Lang (Perimeter Institute) and David W. Hogg (NYU/MPIA/Flatiron)',
+        author_email="dstndstn@gmail.com",
+        distclass=MyDistribution,
+        cmdclass={'build': CustomBuild},
+        version=version,
+        packages=['tractor', 'wise'],
+        package_dir={'wise':'wise', 'tractor':'tractor'},
+        package_data={'wise':['wise-psf-avg.fits', 'allsky-atlas.fits']},
+        ext_modules=mods,
+        py_modules=pymods,
+        zip_safe=False,
     )
-#extra_compile_args=['-O0','-g'],
-#extra_link_args=['-O0', '-g'],
 
-module_em = Extension('tractor._emfit',
-                      sources = ['tractor/emfit.i' ],
-                      include_dirs = numpy_inc,
-                      extra_objects = [],
-                      undef_macros=['NDEBUG'],
-                      )
-
-kwargs = {}
-if os.environ.get('CC') == 'icc':
-    kwargs.update(extra_compile_args=['-g', '-xhost', '-axMIC-AVX512'],
-                  extra_link_args=['-g', '-lsvml'])
-else:
-    kwargs.update(extra_compile_args=['-g', '-std=c99'],
-                  extra_link_args=['-g'])
-
-module_fourier = Extension('tractor._mp_fourier',
-                           sources = ['tractor/mp_fourier.i'],
-                           include_dirs = numpy_inc,
-                           undef_macros=['NDEBUG'],
-                           **kwargs)
-
-module_tsnnls = Extension('tractor._tsnnls',
-                           sources = ['tractor/tsnnls.i'],
-                           include_dirs = numpy_inc,
-                           undef_macros=['NDEBUG'],
-                           **kwargs)
-
-class MyDistribution(Distribution):
-    display_options = Distribution.display_options + [
-        ('with-ceres', None, 'build Ceres module?'),
-        ('with-tsnnls', None, 'build TSNNLS non-neg least-squares module?'),
-        ]
-
-
-## Distutils is so awkward to work with that THIS is the easiest way to add
-# an extra command-line arg!
-
-mods = [module_mix, module_em, module_fourier]
-pymods = ['tractor.mix', 'tractor.emfit', 'tractor.mp_fourier']
-key = '--with-ceres'
-if key in sys.argv:
-    sys.argv.remove(key)
-    mods.append(module_ceres)
-    pymods.append('tractor.ceres')
-key = '--with-tsnnls'
-if key in sys.argv:
-    sys.argv.remove(key)
-    mods.append(module_tsnnls)
-    pymods.append('tractor.tsnnls')
-
-# Record current version number....
-cmd = 'echo "version = \'$(git describe)\'" > tractor/version.py'
-print(cmd)
-os.system(cmd)
-
-setup(
-    distclass=MyDistribution,
-    cmdclass={'build': CustomBuild},
-    name="tractor",
-    version="git",
-    author="Dustin Lang (Perimeter Institute) and David W. Hogg (NYU)",
-    author_email="dstndstn@gmail.com",
-    packages=['tractor', 'wise'],
-    ext_modules = mods,
-    package_data={'wise':['wise-psf-avg.fits', 'allsky-atlas.fits']},
-    package_dir={'wise':'wise', 'tractor':'tractor'},
-    url="http://theTractor.org/",
-    license="GPLv2",
-    description="probabilistic astronomical image analysis",
-    long_description="Attempt at replacing heuristic astronomical catalogs with models built with specified likelihoods, priors, and utilities.",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-    ],
-)
+if __name__ == '__main__':
+    main()
