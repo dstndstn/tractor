c31b269cdebec25e77633ae4488dc844898cdd12
Dustin Lang <dstndstn@gmail.com>
2025-04-11 11:06:56 -0700

    tidy

--- tractor/smarter_dense_optimizer.py
+++ tractor/smarter_dense_optimizer.py
@@ -18,7 +18,6 @@ class SmarterDenseOptimizer(ConstrainedDenseOptimizer):
         assert(variance == False)
         assert(damp == 0.)
 
-
         # Returns: numpy array containing update direction.
         # If *variance* is True, return    (update,variance)
         # If *get_A_matrix* is True, returns the matrix of derivatives.
@@ -63,12 +62,8 @@ class SmarterDenseOptimizer(ConstrainedDenseOptimizer):
                 continue
             live_params.add(iparam)
 
-            #print ("\tLEN DERIV", len(derivs))
             for deriv, img in derivs:
-                #print ("\tIMG ",img)
-                #print('\t\tderiv', deriv, deriv.extent)
                 dx0,dx1,dy0,dy1 = deriv.extent
-
                 if img in img_bounds:
                     x0,x1,y0,y1 = img_bounds[img]
                     img_bounds[img] = min(dx0, x0), max(dx1, x1), min(dy0, y0), max(dy1, y1)
@@ -134,9 +129,8 @@ class SmarterDenseOptimizer(ConstrainedDenseOptimizer):
 
         Nrows = Npixels + Npriors
         if Nrows == 0:
-            #print('ConstrainedDenseOptimizer.getUpdateDirection: Nrows = 0')
             return None
-        #print('Allocating', Nrows, 'x', Ncols, 'matrix for update direction')
+        print('Allocating', Nrows, 'x', Ncols, 'matrix for update direction')
         A = np.zeros((Nrows, Ncols), np.float32)
         # 'B' holds the chi values
         B = np.zeros(Nrows, np.float32)
@@ -145,7 +139,6 @@ class SmarterDenseOptimizer(ConstrainedDenseOptimizer):
         live_params.sort()
 
         column_map = dict([(c,i) for i,c in enumerate(live_params)])
-        #print('Column map:', column_map)
 
         colscales2 = np.ones(Ncols)
         for iparam,derivs in enumerate(allderivs):
@@ -162,31 +155,9 @@ class SmarterDenseOptimizer(ConstrainedDenseOptimizer):
                 x0,x1,y0,y1 = img_bounds[img]
                 if x0 == dx0 and x1 == dx1 and y0 == dy0 and y1 == dy1:
                     rowstart = img_offsets[img]
-                    #print('row start:', rowstart)
-                    #print('col', col)
                     w = x1-x0
                     h = y1-y0
                     apix = (deriv.patch * inverrs[y0:y1, x0:x1])
-                    ### HACK
-                    # ii0,jj0 = np.nonzero(deriv.patch)
-                    # ii1,jj1 = np.nonzero(inverrs[y0:y1, x0:x1])
-                    # if len(ii0) == 0:
-                    #     print('modelmask: deriv is all zeros')
-                    # else:
-                    #     print('modelmask: deriv  non-zero region: x [%i,%i)' % (jj0.min(), jj0.max()+1),
-                    #           'y [%i,%i)' % (ii0.min(), ii0.max()+1))
-                    # if len(ii1) == 0:
-                    #     print('modelmask: inverr is all zeros')
-                    # else:
-                    #     print('modelmask: inverr non-zero region: x [%i,%i)' % (jj1.min(), jj1.max()+1),
-                    #           'y [%i,%i)' % (ii1.min(), ii1.max()+1))
-                    # 
-                    # ii2,jj2 = np.nonzero(inverrs)
-                    # if len(ii2):
-                    #     print('whole      inverr non-zero region (relative to modelmask origin): x [%i,%i)' %
-                    #           (jj2.min() - x0, jj2.max()+1 - x0), 'y [%i,%i)' % (ii2.min()-y0, ii2.max()+1-y0))
-                    # 
-                    ####
                     A[rowstart:rowstart+w*h, col] = apix.flat
                     if scale_columns:
                         # accumulate L2 norm
@@ -197,14 +168,11 @@ class SmarterDenseOptimizer(ConstrainedDenseOptimizer):
                     # (eg from multiple sources), so need to pad it out
                     print('\tderiv extent', dx0,dx1, dy0,dy1)
                     print('\timage bounds', x0,x1,y0,y1, img)
-                    return None
-                    #assert(False)
-                    #sys.exit(-1)
+                    assert(False)
+                    sys.exit(-1)
 
             if scale_columns:
                 colscales2[col] = scale2
-        #print('colscales2:', colscales2)
-        savetxt_cpu_append('ca1.txt', A)
 
         if Npriors > 0:
             rA, cA, vA, pb, mub = priorVals
@@ -219,7 +187,6 @@ class SmarterDenseOptimizer(ConstrainedDenseOptimizer):
                     A[Npixels + rij, col] = vij
                     B[Npixels + rij] += bij
             del priorVals, rA, cA, vA, pb, mub
-        savetxt_cpu_append('ca2.txt', A)
 
         if scale_columns:
             for col,scale2 in enumerate(colscales2):
@@ -229,7 +196,6 @@ class SmarterDenseOptimizer(ConstrainedDenseOptimizer):
                     # Set to safe value...
                     colscales2[col] = 1.
             colscales = np.sqrt(colscales2)
-            #print('colscales:', colscales)
 
         chimap = {}
         if chiImages is not None:
@@ -247,7 +213,6 @@ class SmarterDenseOptimizer(ConstrainedDenseOptimizer):
             h = y1-y0
             B[rowstart: rowstart + w*h] = chi.flat
             del chi
-        savetxt_cpu_append('cb1.txt', B)
 
         try:
             X,_,_,_ = lstsq(A, B, rcond=None)
@@ -265,7 +230,6 @@ class SmarterDenseOptimizer(ConstrainedDenseOptimizer):
         if not get_A_matrix:
             del A
             del B
-        #del B
 
         if scale_columns:
             X /= colscales
